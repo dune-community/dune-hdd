88347f302b6926d34a339161958528df31556082
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-07-05 20:16:40 +0200

    [stationary.linear.elliptic.continuousgalerkin] added dune-fem.hh

---
+++ dune/detailed-solvers/stationary/linear/elliptic/continuousgalerkin/dune-fem.hh
@@ -0,0 +1,39 @@
+#ifndef DUNE_DETAILED_SOLVERS_STATIONARY_LINEAR_ELLIPTIC_CONTINUOUSGALERKIN_DUNE_FEM_HH
+#define DUNE_DETAILED_SOLVERS_STATIONARY_LINEAR_ELLIPTIC_CONTINUOUSGALERKIN_DUNE_FEM_HH
+
+namespace Dune
+{
+
+namespace DetailedSolvers
+{
+
+namespace Stationary
+{
+
+namespace Linear
+{
+
+namespace Elliptic
+{
+
+namespace ContinuousGalerkin
+{
+
+class DuneFem
+{
+
+}; // class DuneFem
+
+} // namespace ContinuousGalerkin
+
+} // namespace Elliptic
+
+} // namespace Linear
+
+} // namespace Stationary
+
+} // namespace DetailedSolvers
+
+} // namespace Dune
+
+#endif // DUNE_DETAILED_SOLVERS_STATIONARY_LINEAR_ELLIPTIC_CONTINUOUSGALERKIN_DUNE_FEM_HH
