84661707ceea1afcd449256e496d00cc1b0934db
Felix Albrecht <felix.albrecht@uni-muenster.de>
2013-01-17 16:33:53 +0100

    [examples...elliptic.cg...] disabled warnings in core modules

--- examples/stationary/linear/elliptic/cg/detailed_discretizations.hh
+++ examples/stationary/linear/elliptic/cg/detailed_discretizations.hh
@@ -1,15 +1,22 @@
-#ifdef HAVE_CMAKE_CONFIG
-  #include "cmake_config.h"
-#elif defined (HAVE_CONFIG_H)
-  #include "config.h"
-#endif // ifdef HAVE_CMAKE_CONFIG
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#pragma GCC diagnostic ignored "-Wtype-limits"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+  #ifdef HAVE_CMAKE_CONFIG
+    #include "cmake_config.h"
+  #elif defined (HAVE_CONFIG_H)
+    #include "config.h"
+  #endif // ifdef HAVE_CMAKE_CONFIG
 
-#include <dune/common/exceptions.hh>
-#include <dune/common/mpihelper.hh>
-#include <dune/common/timer.hh>
+  #include <dune/common/exceptions.hh>
+  #include <dune/common/mpihelper.hh>
+  #include <dune/common/timer.hh>
 
-#include <dune/fem/misc/mpimanager.hh>
-#include <dune/fem/misc/gridwidth.hh>
+  #include <dune/fem/misc/mpimanager.hh>
+  #include <dune/fem/misc/gridwidth.hh>
+#pragma GCC diagnostic push
 
 #include <dune/grid/part/leaf.hh>
 
