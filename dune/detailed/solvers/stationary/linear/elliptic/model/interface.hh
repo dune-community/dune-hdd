eb9dfd5c5e0d09ad4053642dcd3a93fb8bbd0c38
Felix Albrecht <mail@felixalbrecht.de>
2013-02-01 18:50:22 +0100

    [...elliptic.model.interface] (re)added visualize()

--- dune/detailed/solvers/stationary/linear/elliptic/model/interface.hh
+++ dune/detailed/solvers/stationary/linear/elliptic/model/interface.hh
@@ -14,9 +14,8 @@
 #include <dune/grid/io/file/vtk/vtkwriter.hh>
 
 #include <dune/stuff/common/string.hh>
+#include <dune/stuff/common/color.hh>
 #include <dune/stuff/function/interface.hh>
-
-//#include <dune/detailed/solvers/stationary/linear/elliptic/model/interface.hh>
 
 namespace Dune {
 namespace Detailed {
@@ -131,104 +130,109 @@ public:
 //      out << prefix << "  " << paramExplanation()[pp] << ", between " << paramRange()[0](pp) << " and " << paramRange()[1](pp) << std::endl;
 //  }
 
-//  template< class GridViewType >
-//  void visualize(const GridViewType& gridView, std::string filename) const
-//  {
-//    // prepare data
-//    Dune::VTKWriter< GridViewType > vtkWriter(gridView);
-//    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > diffusionPlots
-//        = preparePlot(gridView, *(diffusion()), "diffusion");
-//    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > forcePlots
-//        = preparePlot(gridView, *(force()), "force");
-//    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > dirichletPlots
-//        = preparePlot(gridView, *(dirichlet()), "dirichlet");
-//    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > neumannPlots
-//        = preparePlot(gridView, *(neumann()), "neumann");
-//    // walk the grid view
-//    for (typename GridViewType::template Codim< 0 >::Iterator it = gridView.template begin< 0 >();
-//         it != gridView.template end< 0 >();
-//         ++it) {
-//      const typename GridViewType::template Codim< 0 >::Entity& entity = *it;
-//      const typename GridViewType::IndexSet::IndexType index = gridView.indexSet().index(entity);
-//      const typename DiffusionType::DomainType center = entity.geometry().center();
-//      // do a piecewise constant projection of the data functions
-//      //   * diffusion
-//      if (diffusion()->separable())
-//        for (unsigned int qq = 0; qq < diffusion()->numComponents(); ++qq)
-//          diffusionPlots[qq].second->operator[](index) = diffusion()->components()[qq]->evaluate(center);
-//      else if (!diffusion()->parametric())
-//        diffusionPlots[0].second->operator[](index) = diffusion()->evaluate(center);
-//      else
-//        DUNE_THROW(Dune::InvalidStateException,
-//                   "\nERROR: visualize() not yet implemented for parametric but not separable data functions!");
-//      //   * force
-//      if (force()->separable())
-//        for (unsigned int qq = 0; qq < force()->numComponents(); ++qq)
-//          forcePlots[qq].second->operator[](index) = force()->components()[qq]->evaluate(center);
-//      else if (!force()->parametric())
-//        forcePlots[0].second->operator[](index) = force()->evaluate(center);
-//      else
-//        DUNE_THROW(Dune::InvalidStateException,
-//                   "\nERROR: visualize() not yet implemented for parametric but not separable data functions!");
-//      //   * dirichlet
-//      if (dirichlet()->separable())
-//        for (unsigned int qq = 0; qq < dirichlet()->numComponents(); ++qq)
-//          dirichletPlots[qq].second->operator[](index) = dirichlet()->components()[qq]->evaluate(center);
-//      else if (!dirichlet()->parametric())
-//        dirichletPlots[0].second->operator[](index) = dirichlet()->evaluate(center);
-//      else
-//        DUNE_THROW(Dune::InvalidStateException,
-//                   "\nERROR: visualize() not yet implemented for parametric but not separable data functions!");
-//      //   * neumann
-//      if (neumann()->separable())
-//        for (unsigned int qq = 0; qq < neumann()->numComponents(); ++qq)
-//          neumannPlots[qq].second->operator[](index) = neumann()->components()[qq]->evaluate(center);
-//      else if (!neumann()->parametric())
-//        neumannPlots[0].second->operator[](index) = neumann()->evaluate(center);
-//      else
-//        DUNE_THROW(Dune::InvalidStateException,
-//                   "\nERROR: visualize() not yet implemented for parametric but not separable data functions!");
-//    } // walk the grid view
-//    // write
-//    //   * diffusion
-//    for (unsigned int qq = 0; qq < diffusionPlots.size(); ++qq)
-//      vtkWriter.addCellData(*(diffusionPlots[qq].second), diffusionPlots[qq].first);
-//    //   * force
-//    for (unsigned int qq = 0; qq < forcePlots.size(); ++qq)
-//      vtkWriter.addCellData(*(forcePlots[qq].second), forcePlots[qq].first);
-//    //   * dirichlet
-//    for (unsigned int qq = 0; qq < dirichletPlots.size(); ++qq)
-//      vtkWriter.addCellData(*(dirichletPlots[qq].second), dirichletPlots[qq].first);
-//    //   * neumann
-//    for (unsigned int qq = 0; qq < neumannPlots.size(); ++qq)
-//      vtkWriter.addCellData(*(neumannPlots[qq].second), neumannPlots[qq].first);
-//    vtkWriter.write(filename, Dune::VTK::ascii);
-//  } // void visualize(const GridViewType& gridView, std::string filename) const
+  template< class GridViewType >
+  void visualize(const GridViewType& gridView, std::string filename) const
+  {
+    // prepare data
+    Dune::VTKWriter< GridViewType > vtkWriter(gridView);
+    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > diffusionPlots
+        = preparePlot(gridView, *(diffusion()), "diffusion");
+    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > forcePlots
+        = preparePlot(gridView, *(force()), "force");
+    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > dirichletPlots
+        = preparePlot(gridView, *(dirichlet()), "dirichlet");
+    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > neumannPlots
+        = preparePlot(gridView, *(neumann()), "neumann");
+    // walk the grid view
+    for (typename GridViewType::template Codim< 0 >::Iterator it = gridView.template begin< 0 >();
+         it != gridView.template end< 0 >();
+         ++it) {
+      const typename GridViewType::template Codim< 0 >::Entity& entity = *it;
+      const typename GridViewType::IndexSet::IndexType index = gridView.indexSet().index(entity);
+      const typename FunctionType::DomainType center = entity.geometry().center();
+      // do a piecewise constant projection of the data functions
+      //   * diffusion
+      if (diffusion()->parametric() && diffusion()->separable())
+        for (unsigned int qq = 0; qq < diffusion()->numComponents(); ++qq)
+          diffusionPlots[qq].second->operator[](index) = diffusion()->components()[qq]->evaluate(center);
+      else if (!diffusion()->parametric())
+        diffusionPlots[0].second->operator[](index) = diffusion()->evaluate(center);
+      else
+        DUNE_THROW(Dune::InvalidStateException,
+                   "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                   << " visualize() not yet implemented for parametric but not separable data functions!");
+      //   * force
+      if (force()->parametric() && force()->separable())
+        for (unsigned int qq = 0; qq < force()->numComponents(); ++qq)
+          forcePlots[qq].second->operator[](index) = force()->components()[qq]->evaluate(center);
+      else if (!force()->parametric())
+        forcePlots[0].second->operator[](index) = force()->evaluate(center);
+      else
+        DUNE_THROW(Dune::InvalidStateException,
+                   "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                   << " visualize() not yet implemented for parametric but not separable data functions!");
+      //   * dirichlet
+      if (dirichlet()->parametric() && dirichlet()->separable())
+        for (unsigned int qq = 0; qq < dirichlet()->numComponents(); ++qq)
+          dirichletPlots[qq].second->operator[](index) = dirichlet()->components()[qq]->evaluate(center);
+      else if (!dirichlet()->parametric())
+        dirichletPlots[0].second->operator[](index) = dirichlet()->evaluate(center);
+      else
+        DUNE_THROW(Dune::InvalidStateException,
+                   "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                   << " visualize() not yet implemented for parametric but not separable data functions!");
+      //   * neumann
+      if (neumann()->parametric() && neumann()->separable())
+        for (unsigned int qq = 0; qq < neumann()->numComponents(); ++qq)
+          neumannPlots[qq].second->operator[](index) = neumann()->components()[qq]->evaluate(center);
+      else if (!neumann()->parametric())
+        neumannPlots[0].second->operator[](index) = neumann()->evaluate(center);
+      else
+        DUNE_THROW(Dune::InvalidStateException,
+                   "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                   << " visualize() not yet implemented for parametric but not separable data functions!");
+    } // walk the grid view
+    // write
+    //   * diffusion
+    for (unsigned int qq = 0; qq < diffusionPlots.size(); ++qq)
+      vtkWriter.addCellData(*(diffusionPlots[qq].second), diffusionPlots[qq].first);
+    //   * force
+    for (unsigned int qq = 0; qq < forcePlots.size(); ++qq)
+      vtkWriter.addCellData(*(forcePlots[qq].second), forcePlots[qq].first);
+    //   * dirichlet
+    for (unsigned int qq = 0; qq < dirichletPlots.size(); ++qq)
+      vtkWriter.addCellData(*(dirichletPlots[qq].second), dirichletPlots[qq].first);
+    //   * neumann
+    for (unsigned int qq = 0; qq < neumannPlots.size(); ++qq)
+      vtkWriter.addCellData(*(neumannPlots[qq].second), neumannPlots[qq].first);
+    vtkWriter.write(filename, Dune::VTK::ascii);
+  } // void visualize(const GridViewType& gridView, std::string filename) const
 
-//private:
-//  template< class GridViewType, class FunctionType >
-//  std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > preparePlot(const GridViewType& gridView,
-//                                                                                      const FunctionType& function,
-//                                                                                      const std::string name) const
-//  {
-//    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > ret;
-//    if (function.parametric()) {
-//      if (function.separable()) {
-//        const std::vector< std::string >& paramExplanations = function.paramExplanation();
-//        for (unsigned int qq = 0; qq < function.numComponents(); ++qq)
-//          ret.push_back(std::pair< std::string, std::vector< RangeFieldType >* >(
-//                          name + "_component_" + Dune::Stuff::Common::toString(qq) + ": " + paramExplanations[qq],
-//                          new std::vector< RangeFieldType >(gridView.indexSet().size(0), RangeFieldType(0))));
-//      } else
-//        DUNE_THROW(Dune::InvalidStateException,
-//                   "\nERROR: visualize() not yet implemented for parametric but not separable data functions!");
-//    } else {
-//      ret.push_back(std::pair< std::string, std::vector< RangeFieldType >* >(
-//                      name,
-//                      new std::vector< RangeFieldType >(gridView.indexSet().size(0), RangeFieldType(0))));
-//    }
-//    return ret;
-//  } // std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > preparePlot(...) const
+private:
+  template< class GridViewType, class FunctionType >
+  std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > preparePlot(const GridViewType& gridView,
+                                                                                      const FunctionType& function,
+                                                                                      const std::string name) const
+  {
+    std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > ret;
+    if (function.parametric()) {
+      if (function.separable()) {
+        const std::vector< std::string >& paramExplanations = function.paramExplanation();
+        for (unsigned int qq = 0; qq < function.numComponents(); ++qq)
+          ret.push_back(std::pair< std::string, std::vector< RangeFieldType >* >(
+                          name + "_component_" + Dune::Stuff::Common::toString(qq) + ": " + paramExplanations[qq],
+                          new std::vector< RangeFieldType >(gridView.indexSet().size(0), RangeFieldType(0))));
+      } else
+        DUNE_THROW(Dune::InvalidStateException,
+                   "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
+                   << " visualize() not yet implemented for parametric but not separable data functions!");
+    } else {
+      ret.push_back(std::pair< std::string, std::vector< RangeFieldType >* >(
+                      name,
+                      new std::vector< RangeFieldType >(gridView.indexSet().size(0), RangeFieldType(0))));
+    }
+    return ret;
+  } // std::vector< std::pair< std::string, std::vector< RangeFieldType >* > > preparePlot(...) const
 
 }; // class Interface
 
