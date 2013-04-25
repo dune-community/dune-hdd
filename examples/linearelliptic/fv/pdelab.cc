55fb533f87164371f7b668be6e0b780fc6c4158e
Felix Albrecht <mail@felixalbrecht.de>
2013-04-25 15:13:09 +0200

    [examples.linearelliptic.fv...] update

--- examples/linearelliptic/fv/pdelab.cc
+++ examples/linearelliptic/fv/pdelab.cc
@@ -1,3 +1,9 @@
+#ifdef HAVE_CMAKE_CONFIG
+  #include "cmake_config.h"
+#elif defined (HAVE_CONFIG_H)
+  #include <config.h>
+#endif
+
 #include "pdelab.hh"
 
 #include <boost/filesystem.hpp>
@@ -6,7 +12,7 @@ int main(int argc, char** argv)
 {
   const std::string descriptionFilename = id() + ".description";
   if (!boost::filesystem::exists(descriptionFilename))
-    Problem::writeDescriptionFile(descriptionFilename);
+    ProblemType::writeDescriptionFile(descriptionFilename);
 
   return run(argc, argv);
 } // main
