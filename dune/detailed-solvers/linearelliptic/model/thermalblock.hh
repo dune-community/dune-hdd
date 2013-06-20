1f9cc7d5aba6264e42ac1de3cf57489ab01cc2b4
Felix Albrecht <mail@felixalbrecht.de>
2013-06-20 17:23:28 +0200

    [linearelliptic.model.thermalblock] update, works again

--- dune/detailed-solvers/linearelliptic/model/thermalblock.hh
+++ dune/detailed-solvers/linearelliptic/model/thermalblock.hh
@@ -15,27 +15,27 @@ namespace DetailedSolvers {
 namespace LinearElliptic {
 
 
-template< class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim >
-class ModelThermalblock;
-
-
 template< class DomainFieldImp, int domainDim, class RangeFieldImp >
-class ModelThermalblock< DomainFieldImp, domainDim, RangeFieldImp, 1 >
-  : public ModelDefault< DomainFieldImp, domainDim, RangeFieldImp, 1 >
+class ModelThermalblock< DomainFieldImp, domainDim, RangeFieldImp, 1, true >
+  : public ModelDefault< DomainFieldImp, domainDim, RangeFieldImp, 1, true >
 {
+  typedef ModelDefault< DomainFieldImp, domainDim, RangeFieldImp, 1, true >      BaseType;
 public:
-  typedef ModelThermalblock< DomainFieldImp, domainDim, RangeFieldImp, 1 > ThisType;
-  typedef ModelDefault< DomainFieldImp, domainDim, RangeFieldImp, 1 >      BaseType;
+  typedef ModelThermalblock< DomainFieldImp, domainDim, RangeFieldImp, 1, true > ThisType;
 
   typedef typename BaseType::DomainFieldType  DomainFieldType;
   static const int                            dimDomain = BaseType::dimDomain;
   typedef typename BaseType::RangeFieldType   RangeFieldType;
   static const int                            dimRange = BaseType::dimRange;
 
-  typedef typename BaseType::FunctionType   FunctionType;
+  typedef typename BaseType::ForceType      ForceType;
+  typedef typename BaseType::DirichletType  DirichletType;
+  typedef typename BaseType::NeumannType    NeumannType;
   typedef typename Stuff::FunctionCheckerboard< DomainFieldType, dimDomain, RangeFieldType, dimRange >
                                             CheckerboardFunctionType;
-  typedef typename FunctionType::DomainType DomainType;
+
+  typedef typename CheckerboardFunctionType::DomainType   DomainType;
+
 
   static const std::string id()
   {
@@ -46,9 +46,9 @@ public:
                     const DomainType& _upperRight,
                     const std::vector< size_t >& _numElements,
                     const std::vector< RangeFieldType >& _components,
-                    const std::shared_ptr< const FunctionType > _force,
-                    const std::shared_ptr< const FunctionType > _dirichlet,
-                    const std::shared_ptr< const FunctionType > _neumann)
+                    const std::shared_ptr< const ForceType > _force,
+                    const std::shared_ptr< const DirichletType > _dirichlet,
+                    const std::shared_ptr< const NeumannType > _neumann)
     : BaseType(std::make_shared< CheckerboardFunctionType >(_lowerLeft,
                                                             _upperRight,
                                                             _numElements,
@@ -59,48 +59,33 @@ public:
   {}
 
   ModelThermalblock(const std::shared_ptr< const CheckerboardFunctionType > _diffusion,
-                    const std::shared_ptr< const FunctionType > _force,
-                    const std::shared_ptr< const FunctionType > _dirichlet,
-                    const std::shared_ptr< const FunctionType > _neumann)
+                    const std::shared_ptr< const ForceType > _force,
+                    const std::shared_ptr< const DirichletType > _dirichlet,
+                    const std::shared_ptr< const NeumannType > _neumann)
     : BaseType(_diffusion, _force, _dirichlet, _neumann)
   {}
 
-  ModelThermalblock(const ThisType& _other)
-    :BaseType(_other.diffusion(),
-              _other.force(),
-              _other.dirichlet(),
-              _other.neumann())
-  {}
-
-  ThisType& operator=(const ThisType& _other)
-  {
-    if (this != &_other) {
-      BaseType::operator=(_other);
-    }
-    return this;
-  } // ThisType& operator=(const ThisType& other)
-
-  static Dune::ParameterTree createSampleDescription(const std::string subName = "")
+  static Dune::ParameterTree defaultSettings(const std::string subName = "")
   {
     Dune::Stuff::Common::ExtendedParameterTree description;
     description.add(Dune::Stuff::Functions< DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRange >::createSampleDescription("function.checkerboard"),
+                                            RangeFieldType, dimRange >::defaultSettings("function.checkerboard"),
                     "diffusion");
     description["diffusion.name"] = "diffusion";
     description.add(Dune::Stuff::Functions< DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRange >::createSampleDescription("function.expression"),
+                                            RangeFieldType, dimRange >::defaultSettings("function.expression"),
                     "force");
     description["force.name"] = "force";
     description["force.expression"] = "1.0";
     description["force.order"] = "0";
     description.add(Dune::Stuff::Functions< DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRange >::createSampleDescription("function.expression"),
+                                            RangeFieldType, dimRange >::defaultSettings("function.expression"),
                     "neumann");
     description["neumann.name"] = "neumann";
     description["neumann.expression"] = "0.1";
     description["neumann.order"] = "0";
     description.add(Dune::Stuff::Functions< DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRange >::createSampleDescription("function.expression"),
+                                            RangeFieldType, dimRange >::defaultSettings("function.expression"),
                     "dirichlet");
     description["dirichlet.name"] = "dirichlet";
     description["dirichlet.expression"] = "0.1*x[0]";
@@ -114,40 +99,23 @@ public:
     }
   }
 
-  static ThisType* create(const Dune::ParameterTree& _description, const std::string _subName = id())
+  static ThisType* create(const Dune::ParameterTree& _settings, const std::string _subName = id())
   {
     // get correct paramTree
-    Dune::Stuff::Common::ExtendedParameterTree description;
-    if (_description.hasSub(_subName))
-      description = _description.sub(_subName);
+    Dune::Stuff::Common::ExtendedParameterTree settings;
+    if (_settings.hasSub(_subName))
+      settings = _settings.sub(_subName);
     else
-      description = _description;
+      settings = _settings;
     // create the correct diffusion
-    const Dune::Stuff::Common::ExtendedParameterTree& diffusionDescription = description.sub("diffusion");
-    const std::shared_ptr< const CheckerboardFunctionType >
-        diffusion(CheckerboardFunctionType::create(diffusionDescription));
-    return new ThisType(diffusion,
-                        createFunction("force", description),
-                        createFunction("dirichlet", description),
-                        createFunction("neumann", description));
+    return new ThisType(std::shared_ptr< CheckerboardFunctionType >(CheckerboardFunctionType::create(settings.sub("diffusion"))),
+                        BaseType::template createFunction< ForceType >("force", settings),
+                        BaseType::template createFunction< DirichletType >("dirichlet", settings),
+                        BaseType::template createFunction< NeumannType >("neumann", settings));
   } // static ThisType createFromParamTree(const Dune::ParameterTree& paramTree, const std::string subName = id())
 
 private:
-  static std::shared_ptr< const FunctionType > createFunction(const std::string& _id,
-                                                               const Dune::Stuff::Common::ExtendedParameterTree& _description)
-  {
-    const Dune::Stuff::Common::ExtendedParameterTree functionDescription = _description.sub(_id);
-    std::string type;
-    // we do this instead of using get() with 'function.expression' as a default in order to prevent get() from
-    // throwing a warning
-    if (functionDescription.hasKey("type"))
-      type = functionDescription.get< std::string >("type");
-    else
-      type = "function.expression";
-    return std::shared_ptr< const FunctionType >(Dune::Stuff::Functions<  DomainFieldType, dimDomain,
-                                                                          RangeFieldType, dimRange >::create(type,
-                                                                                                             functionDescription));
-  }
+
 }; // class ModelThermalblock
 
 
