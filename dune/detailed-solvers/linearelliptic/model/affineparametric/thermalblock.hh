2d94d3b54d0ca003b99b25f269ecdb07b2f83dd5
Felix Albrecht <mail@felixalbrecht.de>
2013-06-21 19:39:46 +0200

    [linearelliptic.model.affineparametric.thermalblock] updated to new layout

--- dune/detailed-solvers/linearelliptic/model/affineparametric/thermalblock.hh
+++ dune/detailed-solvers/linearelliptic/model/affineparametric/thermalblock.hh
@@ -18,17 +18,17 @@ namespace LinearElliptic {
 
 
 // forward, to allow for specialization
-template< class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim >
+template< class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim, bool scalarDiffusion = true >
 class ModelAffineParametricThermalblock;
 
 
 template< class DomainFieldImp, int domainDim, class RangeFieldImp >
-class ModelAffineParametricThermalblock< DomainFieldImp, domainDim, RangeFieldImp, 1 >
-  : public ModelAffineParametricDefault< DomainFieldImp, domainDim, RangeFieldImp, 1 >
+class ModelAffineParametricThermalblock< DomainFieldImp, domainDim, RangeFieldImp, 1, true >
+  : public ModelAffineParametricDefault< DomainFieldImp, domainDim, RangeFieldImp, 1, true >
 {
+  typedef ModelAffineParametricDefault< DomainFieldImp, domainDim, RangeFieldImp, 1, true >       BaseType;
 public:
-  typedef ModelAffineParametricThermalblock< DomainFieldImp, domainDim, RangeFieldImp, 1 > ThisType;
-  typedef ModelAffineParametricDefault< DomainFieldImp, domainDim, RangeFieldImp, 1 >      BaseType;
+  typedef ModelAffineParametricThermalblock< DomainFieldImp, domainDim, RangeFieldImp, 1, true >  ThisType;
 
   typedef typename BaseType::DomainFieldType  DomainFieldType;
   static const int                            dimDomain = BaseType::dimDomain;
@@ -37,10 +37,12 @@ public:
   typedef typename BaseType::ParamFieldType   ParamFieldType;
   static const int                            maxParamDim = BaseType::maxParamDim;
 
-  typedef typename Dune::Stuff::FunctionAffineParametricCheckerboard< DomainFieldType, dimDomain,
+  typedef typename Dune::Stuff::AffineParametricFunctionCheckerboard< DomainFieldType, dimDomain,
                                                                       RangeFieldType, dimRange >
                                           CheckerboardFunctionType;
-  typedef typename BaseType::FunctionType FunctionType;
+  typedef typename BaseType::ForceType      ForceType;
+  typedef typename BaseType::DirichletType  DirichletType;
+  typedef typename BaseType::NeumannType    NeumannType;
 
   static const std::string id()
   {
@@ -48,48 +50,33 @@ public:
   }
 
   ModelAffineParametricThermalblock(const Dune::shared_ptr< const CheckerboardFunctionType > _diffusion,
-                                    const Dune::shared_ptr< const FunctionType > _force,
-                                    const Dune::shared_ptr< const FunctionType > _dirichlet,
-                                    const Dune::shared_ptr< const FunctionType > _neumann)
+                                    const Dune::shared_ptr< const ForceType > _force,
+                                    const Dune::shared_ptr< const DirichletType > _dirichlet,
+                                    const Dune::shared_ptr< const NeumannType > _neumann)
     : BaseType(_diffusion, _force, _dirichlet, _neumann)
   {}
 
-  ModelAffineParametricThermalblock(const ThisType& other)
-    : BaseType(other.diffusion(),
-               other.force(),
-               other.dirichlet(),
-               other.neumann())
-  {}
-
-  ThisType& operator=(const ThisType& other)
-  {
-    if (this != &other) {
-      BaseType::operator=(other);
-    }
-    return *this;
-  }
-
-  static Dune::ParameterTree createSampleDescription(const std::string subName = "")
+  static Dune::ParameterTree defaultSettings(const std::string subName = "")
   {
     Dune::Stuff::Common::ExtendedParameterTree description;
-    description.add(Dune::Stuff::Functions< DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRange >::createSampleDescription("function.affineparametric.checkerboard"),
+    description.add(Dune::Stuff::AffineParametricFunctions< DomainFieldType, dimDomain,
+                                                            RangeFieldType, dimRange >::defaultSettings("function.affineparametric.checkerboard"),
                     "diffusion");
     description["diffusion.name"] = "diffusion";
     description.add(Dune::Stuff::Functions< DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRange >::createSampleDescription("function.expression"),
+                                            RangeFieldType, dimRange >::defaultSettings("function.expression"),
                     "force");
     description["force.name"] = "force";
     description["force.expression"] = "1.0";
     description["force.order"] = "0";
     description.add(Dune::Stuff::Functions< DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRange >::createSampleDescription("function.expression"),
+                                            RangeFieldType, dimRange >::defaultSettings("function.expression"),
                     "neumann");
     description["neumann.name"] = "neumann";
     description["neumann.expression"] = "0.1";
     description["neumann.order"] = "0";
     description.add(Dune::Stuff::Functions< DomainFieldType, dimDomain,
-                                            RangeFieldType, dimRange >::createSampleDescription("function.expression"),
+                                            RangeFieldType, dimRange >::defaultSettings("function.expression"),
                     "dirichlet");
     description["dirichlet.name"] = "dirichlet";
     description["dirichlet.expression"] = "0.1*x[0]";
@@ -101,35 +88,35 @@ public:
       extendedDescription.add(description, subName);
       return extendedDescription;
     }
-  }
+  } // ... defaultSettings(...)
 
-  static ThisType* create(const Dune::ParameterTree& _description, const std::string _subName = id())
+  static ThisType* create(const Dune::ParameterTree& _settings, const std::string _subName = id())
   {
-    // get correct description
-    typedef Stuff::Common::ExtendedParameterTree DescriptionType;
-    DescriptionType description;
-    if (_description.hasSub(_subName))
-      description = _description.sub(_subName);
+    // get correct settings
+    typedef Stuff::Common::ExtendedParameterTree SettingsType;
+    SettingsType settings;
+    if (_settings.hasSub(_subName))
+      settings = _settings.sub(_subName);
     else
-      description = _description;
+      settings = _settings;
     // create the checkerboard diffusion
-    DescriptionType diffusionDescription = description.sub("diffusion");
-    diffusionDescription["diffusion.name"] = "checkerboard diffusion";
+    SettingsType diffusionSettings = settings.sub("diffusion");
+    diffusionSettings["diffusion.name"] = "checkerboard diffusion";
     const shared_ptr< const CheckerboardFunctionType >
-        diffusion(CheckerboardFunctionType::create(diffusionDescription));
+        diffusion(CheckerboardFunctionType::create(diffusionSettings));
     // create the rest of the functions
-    //   * therefore create a fake diffusion subdescription,
-    description.sub("diffusion") = CheckerboardFunctionType::createSampleDescription();
-    description["diffusion.type"] = "function.affineparametric.checkerboard";
-    description["diffusion.name"] = "checkerboard diffusion";
+    //   * therefore create a fake diffusion subSettings,
+    settings.sub("diffusion") = CheckerboardFunctionType::defaultSettings();
+    settings["diffusion.type"] = "function.affineparametric.checkerboard";
+    settings["diffusion.name"] = "checkerboard diffusion";
     //   * create a default model
-    const std::shared_ptr< const BaseType > baseModel(BaseType::create(description));
+    const std::shared_ptr< const BaseType > baseModel(BaseType::create(settings));
     // create and return
     return new ThisType(diffusion,
                         baseModel->force(),
                         baseModel->dirichlet(),
                         baseModel->neumann());
-  } // static ThisType createFromParamTree(const Dune::ParameterTree& paramTree, const std::string subName = id())
+  } // static ThisType create(...)
 }; // class ModelAffineParametricThermalblock
 
 
