47732e2212e99428d4b7d0be97f5cbd406579901
Felix Schindler <felix@schindlerfamily.de>
2014-04-30 14:48:46 +0200

    [examples.linearelliptic.cg] updated python main

--- examples/linearelliptic/cg_main.py
+++ examples/linearelliptic/cg_main.py
@@ -12,43 +12,38 @@ try:
     import linearellipticexamplecg as example_module
 
     # create example and write settings file
-    example = example_module.LinearellipticExampleCG__DuneSGrid__lt___2__2___gt___1()
-    settingsFilename = example.static_id() + '.settings'
+    example = example_module.LinearellipticExampleCG__DuneSGrid__lt___2__2___gt__()
+    settingsFilename = example.static_id() + '.cfg'
     if not os.path.exists(settingsFilename):
-        example.write_settings_file()
+        example.write_config_file()
     # initialize everything (grid, problem, discretization)
     example.initialize([os.getcwd()])
     discretization = example.discretization()
-    # inspect the discretization
-    available_operators = list(discretization.available_operators())
-    print('discretization provides:')
-    for operator_id in available_operators:
-        operator = discretization.get_operator(operator_id)
-        print('  operator \'{}\' with parameter type: {}'.format(operator_id,
-                                                                 operator.parameter_type().report()))
-    available_functionals = list(discretization.available_functionals())
-    for functional_id in available_functionals:
-        functional = discretization.get_functional(functional_id)
-        print('  functional \'{}\' with parameter type: {}'.format(functional_id,
-                                                                   functional.parameter_type().report()))
-    available_solver_contexts = list(discretization.solver_options())
-    for solver_context in available_solver_contexts:
-        solver_options = discretization.solver_options(context)
-        print('  solver context \'{}\': \'{}\''.format(solver_context, solver_options))
-    # solve
-    mu = example_module.Dune.Pymor.Parameter('diffusion', [0.1, 1.0, 0.1, 1.0])
-    print('')
-    print('solving for parameter mu = {}... '.format(mu.report()), end='')
-    vector = discretization.create_vector()
-    discretization.solve(vector, mu)
-    print('done')
-    filename = 'solution'
-    print('writing to {}.vtu... '.format(filename), end='')
-    discretization.visualize(vector, filename, 'CG solution')
-    print('done')
+    if discretization.parametric():
+        # solve
+        mu = example_module.Dune.Pymor.Parameter('diffusion_factor', [0.1, 1.0, 0.1, 1.0])
+        print('')
+        print('solving for parameter mu = {}... '.format(mu.report()), end='')
+        vector = discretization.create_vector()
+        discretization.solve(vector, mu)
+        print('done')
+        filename = 'solution'
+        print('writing to {}.vtu... '.format(filename), end='')
+        discretization.visualize(vector, filename, 'CG solution')
+        print('done')
+    else:
+        print('solving ... ', end='')
+        vector = discretization.create_vector()
+        discretization.solve(vector)
+        print('done')
+        filename = 'solution'
+        print('writing to {}.vtu... '.format(filename), end='')
+        discretization.visualize(vector, filename, 'CG solution')
+        print('done')
 
 except ImportError:
     print('There was an error importing \'linearellipticexamplecg\'!')
-    print('Did you build the example successfully,')
-    print('i.e. did \'make linearellipticexamplecg\' succeed?')
+    print('Did you build the example successfully, i.e. did')
+    print('  make linearellipticexamplecg')
+    print('succeed?')
     raise
