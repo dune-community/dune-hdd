8cf6e2209048336cac6fdbe2ef61925238111e40
Felix Schindler <felix@schindlerfamily.de>
2014-05-12 15:56:13 +0200

    [linearelliptic.problems] refactor ProblemProvider

--- dune/hdd/linearelliptic/problems.hh
+++ dune/hdd/linearelliptic/problems.hh
@@ -16,56 +16,115 @@
 #include "problems/interfaces.hh"
 #include "../playground/linearelliptic/problems/default.hh"
 #include "../playground/linearelliptic/problems/ESV2007.hh"
+#include "../playground/linearelliptic/problems/mixed-boundaries.hh"
 #include "../playground/linearelliptic/problems/thermalblock.hh"
 
 namespace Dune {
 namespace HDD {
 namespace LinearElliptic {
+namespace internal {
 
 
 template< class E, class D, int d, class R, int r = 1 >
-class ProblemProvider
+class ProblemProviderBase
 {
 public:
   typedef ProblemInterface< E, D, d, R, r > InterfaceType;
 
+protected:
+  template< class ProblemType >
+  static std::unique_ptr< ProblemType > call_create(const Stuff::Common::ConfigTree& config)
+  {
+    if (config.empty())
+      return ProblemType::create();
+    else
+      return ProblemType::create(config);
+  } // ... call_create(...)
+
+public:
   static std::vector< std::string > available()
   {
     return {
         Problems::Default< E, D, d, R, r >::static_id()
-      , Problems::ESV2007< E, D, d, R, r >::static_id()
       , Problems::Thermalblock< E, D, d, R, r >::static_id()
+      , Problems::MixedBoundaries< E, D, d, R, r >::static_id()
     };
-  }
+  } // ... available(...)
 
   static Stuff::Common::ConfigTree default_config(const std::string type = available()[0],
                                                   const std::string sub_name = "")
   {
   if (type == Problems::Default< E, D, d, R, r >::static_id())
     return Problems::Default< E, D, d, R, r >::default_config(sub_name);
-  else if (type == Problems::ESV2007< E, D, d, R, r >::static_id())
-    return Problems::ESV2007< E, D, d, R, r >::default_config(sub_name);
   else if (type == Problems::Thermalblock< E, D, d, R, r >::static_id())
     return Problems::Thermalblock< E, D, d, R, r >::default_config(sub_name);
+  else if (type == Problems::MixedBoundaries< E, D, d, R, r >::static_id())
+    return Problems::MixedBoundaries< E, D, d, R, r >::default_config(sub_name);
   else
     DUNE_THROW_COLORFULLY(Stuff::Exceptions::wrong_input_given,
                           "'" << type << "' is not a valid " << InterfaceType::static_id() << "!");
-  }
+  } // ... default_config(...)
 
   static std::unique_ptr< InterfaceType > create(const std::string type = available()[0],
                                                  const Stuff::Common::ConfigTree config = default_config(available()[0]))
   {
     if (type == Problems::Default< E, D, d, R, r >::static_id())
-      return Problems::Default< E, D, d, R, r >::create(config);
-    else if (type == Problems::ESV2007< E, D, d, R, r >::static_id())
-      return Problems::ESV2007< E, D, d, R, r >::create(config);
+      return call_create< Problems::Default< E, D, d, R, r > >(config);
     else if (type == Problems::Thermalblock< E, D, d, R, r >::static_id())
-      return Problems::Thermalblock< E, D, d, R, r >::create(config);
+      return call_create< Problems::Thermalblock< E, D, d, R, r > >(config);
+    else if (type == Problems::MixedBoundaries< E, D, d, R, r >::static_id())
+      return call_create< Problems::MixedBoundaries< E, D, d, R, r > >(config);
     else
       DUNE_THROW_COLORFULLY(Stuff::Exceptions::wrong_input_given,
                             "'" << type << "' is not a valid " << InterfaceType::static_id() << "!");
-  }
-}; // clas ProblemProvider
+  } // ... create(...)
+}; // clas ProblemProviderBase
+
+
+} // namespace internal
+
+
+template< class E, class D, int d, class R, int r = 1 >
+class ProblemProvider
+  : public internal::ProblemProviderBase< E, D, d, R, r >
+{};
+
+
+template< class E, class D, class R >
+class ProblemProvider< E, D, 2, R, 1 >
+  : public internal::ProblemProviderBase< E, D, 2, R, 1 >
+{
+  static const unsigned int d = 2;
+  static const unsigned int r = 1;
+  typedef internal::ProblemProviderBase< E, D, d, R, r > BaseType;
+public:
+  using typename BaseType::InterfaceType;
+
+  static std::vector< std::string > available()
+  {
+    auto base = BaseType::available();
+    base.push_back(Problems::ESV2007< E, D, d, R, r >::static_id());
+    return base;
+  } // ... available(...)
+
+  static Stuff::Common::ConfigTree default_config(const std::string type = available()[0],
+                                                  const std::string sub_name = "")
+  {
+  if (type == Problems::ESV2007< E, D, d, R, r >::static_id())
+    return Problems::ESV2007< E, D, d, R, r >::default_config(sub_name);
+  else
+    return BaseType::default_config(type, sub_name);
+  } // ... default_config(...)
+
+  static std::unique_ptr< InterfaceType > create(const std::string type = available()[0],
+                                                 const Stuff::Common::ConfigTree config = Stuff::Common::ConfigTree())
+  {
+    if (type == Problems::ESV2007< E, D, d, R, r >::static_id())
+      return BaseType::template call_create< Problems::ESV2007< E, D, d, R, r > >(config);
+    else
+      return BaseType::create(type, config);
+  } // ... create(...)
+}; // clas ProblemProvider< ..., 2, ... 1 >
 
 
 } // namespace LinearElliptic
