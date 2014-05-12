3a9ab67eb1edd831e1dc5466cf7eaa354cd66cb2
Felix Schindler <felix@schindlerfamily.de>
2014-05-12 15:53:06 +0200

    [linearelliptic.problems.interfaces] added report() and operator<<

--- dune/hdd/linearelliptic/problems/interfaces.hh
+++ dune/hdd/linearelliptic/problems/interfaces.hh
@@ -7,6 +7,7 @@
 #define DUNE_HDD_LINEARELLIPTIC_PROBLEMS_INTERFACES_HH
 
 #include <vector>
+#include <ostream>
 
 #include <dune/grid/common/gridview.hh>
 #include <dune/grid/io/file/vtk.hh>
@@ -27,6 +28,7 @@ template< class EntityImp, class DomainFieldImp, int domainDim, class RangeField
 class ProblemInterface
   : public Pymor::Parametric
 {
+  typedef ProblemInterface< EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim > ThisType;
 public:
   typedef EntityImp EntityType;
   typedef DomainFieldImp    DomainFieldType;
@@ -89,6 +91,19 @@ public:
     }
   } // ... visualize(...) const
 
+  virtual void report(std::ostream& out, std::string prefix = "") const
+  {
+    out << prefix << "problem '" << type() << "':" << std::endl;
+    out << prefix << "  diffusion_factor:" << std::endl;
+    diffusion_factor().report(out, prefix + "    ");
+    out << "\n" << prefix << "  force:" << std::endl;
+    force().report(out, prefix + "    ");
+    out << "\n"<< prefix << "  dirichlet:" << std::endl;
+    dirichlet().report(out, prefix + "    ");
+    out << "\n" << prefix << "  neumann:" << std::endl;
+    neumann().report(out, prefix + "    ");
+  } // ... report(...)
+
 private:
   template< class GridViewType, class VTKWriterType >
   void add_visualizations_(const GridViewType& grid_view, VTKWriterType& vtk_writer) const
@@ -108,7 +123,19 @@ private:
     if (function.has_affine_part())
       vtk_writer.addVertexData(std::make_shared< VisualizationAdapter >(*(function.affine_part())));
   } // ... add_function_visualization_(...)
+
+private:
+  template< class T >
+  friend std::ostream& operator<<(std::ostream& /*out*/, const ThisType& /*problem*/);
 }; // ProblemInterface
+
+
+template< class E, class D, int d, class R, int r >
+std::ostream& operator<<(std::ostream& out, const ProblemInterface< E, D, d, R, r >& problem)
+{
+  problem.report(out);
+  return out;
+} // ... operator<<(...)
 
 
 } // namespace LinearElliptic
