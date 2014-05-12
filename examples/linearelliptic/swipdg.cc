4c62da2610a66c9344317f4a974b3c8c2cd69247
Felix Schindler <felix@schindlerfamily.de>
2014-05-12 15:58:32 +0200

    [examples.linearelliptic.swipdg] added first draft

---
+++ examples/linearelliptic/swipdg.cc
@@ -0,0 +1,16 @@
+// This file is part of the dune-hdd project:
+//   http://users.dune-project.org/projects/dune-hdd
+// Copyright holders: Felix Albrecht
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#include "config.h"
+
+#include "swipdg.hh"
+
+#if HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
+# define ENABLE_ALUGRID 1
+# include <dune/grid/alugrid.hh>
+
+template class LinearellipticExampleSWIPDG< Dune::ALUConformGrid< 2, 2 > >;
+
+#endif // HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
---
+++ examples/linearelliptic/swipdg.hh
@@ -0,0 +1,106 @@
+// This file is part of the dune-hdd project:
+//   http://users.dune-project.org/projects/dune-hdd
+// Copyright holders: Felix Albrecht
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_HDD_EXAMPLES_LINEARELLIPTIC_SWIPDG_HH
+#define DUNE_HDD_EXAMPLES_LINEARELLIPTIC_SWIPDG_HH
+
+#include "config.h"
+
+#include <memory>
+
+#include <dune/stuff/common/memory.hh>
+
+#include <dune/hdd/playground/linearelliptic/discreteproblem.hh>
+#include <dune/hdd/playground/linearelliptic/discretizations/swipdg.hh>
+
+template< class GridImp >
+class LinearellipticExampleSWIPDG
+{
+public:
+  typedef GridImp GridType;
+  typedef Dune::HDD::LinearElliptic::DiscreteProblem< GridType > DiscreteProblemType;
+  typedef typename DiscreteProblemType::RangeFieldType RangeFieldType;
+  static const unsigned int dimRange = DiscreteProblemType::dimRange;
+  typedef Dune::HDD::LinearElliptic::Discretizations::SWIPDG< GridType, Dune::Stuff::Grid::ChooseLayer::leaf,
+                                                              RangeFieldType, dimRange, 1 > DiscretizationType;
+
+  static std::string static_id()
+  {
+    return "linearelliptic.swipdg";
+  }
+
+  static void write_config_file(const std::string filename = static_id() + ".cfg")
+  {
+    DiscreteProblemType::write_config(filename, static_id());
+  }
+
+  void initialize(const std::vector< std::string > arguments)
+  {
+    using namespace Dune;
+    if (!discrete_problem_) {
+      discrete_problem_ = Stuff::Common::make_unique< DiscreteProblemType >(static_id(), arguments);
+      const bool debug_logging = discrete_problem_->debug_logging();
+      auto& info = DSC_LOG_INFO;
+      auto& debug = DSC_LOG_DEBUG;
+      discretization_ = Stuff::Common::make_unique< DiscretizationType >(discrete_problem_->grid_provider(),
+                                                                         discrete_problem_->boundary_info(),
+                                                                         discrete_problem_->problem());
+      info << "initializing discretization";
+      if (debug_logging)
+        info << ":" << std::endl;
+      else
+        info << "... " << std::flush;
+      Dune::Timer timer;
+      discretization_->init(debug, "  ");
+      if (!debug_logging)
+        info << "done (took " << timer.elapsed() << "s)" << std::endl;
+    } else
+      DSC_LOG_INFO << "initialize has already been called" << std::endl;
+  } // ... initialize(...)
+
+  const DiscreteProblemType& discrete_problem() const
+  {
+    return *discrete_problem_;
+  }
+
+  const DiscretizationType& discretization() const
+  {
+    return *discretization_;
+  }
+
+  DiscretizationType* discretization_and_return_ptr() const
+  {
+    return new DiscretizationType(*discretization_);
+  }
+
+private:
+  std::unique_ptr< DiscreteProblemType > discrete_problem_;
+  std::unique_ptr< DiscretizationType > discretization_;
+}; // class LinearellipticExampleSWIPDG
+
+
+namespace Dune {
+
+
+// forward grid declaratios
+template< int dim, int dimworld, typename _ctype >
+class SGrid;
+
+template< int dim >
+class YaspGrid;
+
+
+} // namespace Dune
+
+
+extern template class LinearellipticExampleSWIPDG< Dune::SGrid< 1, 1 > >;
+extern template class LinearellipticExampleSWIPDG< Dune::SGrid< 2, 2 > >;
+extern template class LinearellipticExampleSWIPDG< Dune::SGrid< 3, 3 > >;
+
+extern template class LinearellipticExampleSWIPDG< Dune::YaspGrid< 1 > >;
+extern template class LinearellipticExampleSWIPDG< Dune::YaspGrid< 2 > >;
+extern template class LinearellipticExampleSWIPDG< Dune::YaspGrid< 3 > >;
+
+#endif // DUNE_HDD_EXAMPLES_LINEARELLIPTIC_SWIPDG_HH
