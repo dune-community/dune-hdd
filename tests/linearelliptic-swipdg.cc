ee04ed9fd323f7496a3329082b322fa50c92fb5f
Felix Schindler <felix@schindlerfamily.de>
2014-05-29 12:52:20 +0200

    [tests.linearelliptic-swipdg] added first draft
    This is basically a copy from dune-gdt.

---
+++ tests/linearelliptic-swipdg.cc
@@ -0,0 +1,104 @@
+// This file is part of the dune-hdd project:
+//   http://users.dune-project.org/projects/dune-hdd
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#define DUNE_STUFF_FUNCTIONS_DISABLE_CHECKS
+
+// This one has to come first (includes the config.h)!
+#include <dune/stuff/test/test_common.hh>
+
+#if HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
+# define ENABLE_ALUGRID 1
+# include <dune/grid/alugrid.hh>
+#else
+# error This test requires ALUGrid!
+#endif
+
+#include <dune/stuff/common/exceptions.hh>
+
+#include <dune/stuff/common/print.hh>
+#include <dune/stuff/common/float_cmp.hh>
+
+#include <dune/hdd/playground/linearelliptic/testcases/ESV2007.hh>
+
+#include "linearelliptic-swipdg.hh"
+
+using namespace Dune;
+using namespace HDD;
+
+
+// change this to toggle output
+std::ostream& test_out = std::cout;
+//std::ostream& test_out = DSC_LOG.devnull();
+
+
+class errors_are_not_as_expected
+  : public Dune::Exception
+{};
+
+std::vector< double > truncate_vector(const std::vector< double >& in, const size_t size)
+{
+  assert(size <= in.size());
+  if (size == in.size())
+    return in;
+  else {
+    std::vector< double > ret(size);
+    for (size_t ii = 0; ii < size; ++ii)
+      ret[ii] = in[ii];
+    return ret;
+  }
+} // ... truncate_vector(...)
+
+
+typedef Dune::ALUGrid< 2, 2, Dune::simplex, Dune::conforming > AluConform2dGridType;
+
+typedef testing::Types< LinearElliptic::TestCases::ESV2007< AluConform2dGridType >
+                      > AluConform2dTestCases;
+
+
+template< class TestCaseType >
+struct linearelliptic_SWIPDG_discretization
+  : public ::testing::Test
+{
+  static void eoc_study()
+  {
+    const TestCaseType test_case;
+    test_case.print_header(test_out);
+    test_out << std::endl;
+    LinearElliptic::Tests::EocStudySWIPDG< TestCaseType, 1 > eoc_study(test_case);
+    auto errors = eoc_study.run(test_out);
+    for (const auto& norm : eoc_study.provided_norms())
+      if (!Dune::Stuff::Common::FloatCmp::lt(errors[norm],
+                                             truncate_vector(eoc_study.expected_results(norm), errors[norm].size()))) {
+        std::stringstream ss;
+        Dune::Stuff::Common::print(errors[norm],                        "errors           (" + norm + ")", ss);
+        Dune::Stuff::Common::print(eoc_study.expected_results(norm), "   expected results (" + norm + ")", ss);
+        DUNE_THROW(errors_are_not_as_expected, ss.str());
+      }
+  } // ... eoc_study()
+}; // linearelliptic_SWIPDG_discretization
+
+
+TYPED_TEST_CASE(linearelliptic_SWIPDG_discretization, AluConform2dTestCases);
+TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study) {
+  this->eoc_study();
+}
+
+
+int main(int argc, char** argv)
+{
+  try {
+    test_init(argc, argv);
+    return RUN_ALL_TESTS();
+  } catch (Dune::Exception& e) {
+    std::cerr << "\nDune reported error: " << e.what() << std::endl;
+    std::abort();
+  } catch (std::exception& e) {
+    std::cerr << "\n" << e.what() << std::endl;
+    std::abort();
+  } catch (...) {
+    std::cerr << "Unknown exception thrown!" << std::endl;
+    std::abort();
+  } // try
+}
---
+++ tests/linearelliptic-swipdg.hh
@@ -0,0 +1,190 @@
+﻿// This file is part of the dune-hdd project:
+//   http://users.dune-project.org/projects/dune-hdd
+// Copyright holders: Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_HDD_TEST_LINEARELLIPTIC_SWIPDG_HH
+#define DUNE_HDD_TEST_LINEARELLIPTIC_SWIPDG_HH
+
+#include <dune/stuff/common/exceptions.hh>
+
+#include <dune/gdt/products/l2.hh>
+#include <dune/gdt/products/h1.hh>
+
+#include <dune/hdd/linearelliptic/discretizations/swipdg.hh>
+#include <dune/hdd/playground/linearelliptic/testcases/ESV2007.hh>
+
+#include "linearelliptic.hh"
+
+namespace Dune {
+namespace HDD {
+namespace LinearElliptic {
+namespace Tests {
+namespace internal {
+
+
+template< class TestCaseType, int polOrder >
+class Discretization
+{
+  typedef typename TestCaseType::GridType GridType;
+  typedef typename TestCaseType::RangeFieldType RangeFieldType;
+  static const unsigned int dimRange = TestCaseType::dimRange;
+public:
+  typedef Discretizations::SWIPDG< GridType, Stuff::Grid::ChooseLayer::level, RangeFieldType, dimRange, polOrder > Type;
+}; // class Discretization
+
+
+} // namespace internal
+
+
+template< class TestCaseType, int polOrder >
+class EocStudySWIPDG
+  : public EocStudyBase< TestCaseType, typename internal::Discretization< TestCaseType, polOrder >::Type >
+{
+  typedef EocStudyBase< TestCaseType, typename internal::Discretization< TestCaseType, polOrder >::Type > BaseType;
+
+  typedef typename BaseType::DiscretizationType DiscretizationType;
+  typedef typename BaseType::GridViewType GridViewType;
+  typedef typename BaseType::FunctionType FunctionType;
+
+public:
+  EocStudySWIPDG(const TestCaseType& test_case)
+    : BaseType(test_case)
+  {}
+
+  virtual ~EocStudySWIPDG() {}
+
+  virtual std::string identifier() const DS_OVERRIDE DS_FINAL
+  {
+    return DiscretizationType::static_id() + " (polorder " + Stuff::Common::toString(polOrder) + ")";
+  }
+
+  virtual std::vector< std::string > provided_norms() const DS_OVERRIDE DS_FINAL
+  {
+    return {"L2", "H1_semi"};
+  }
+
+  virtual size_t expected_rate(const std::string type) const DS_OVERRIDE DS_FINAL
+  {
+    if (type.compare("L2") == 0)
+      return polOrder + 1;
+    else if (type.compare("H1_semi") == 0)
+      return polOrder;
+    else
+      DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+  } // ... expected_rate(...)
+
+  std::vector< double > expected_results(const std::string type) const
+  {
+    if (std::is_same< TestCaseType, TestCases::ESV2007< ALUConformGrid< 2, 2 > > >::value
+        || std::is_same< TestCaseType, TestCases::ESV2007< ALUGrid< 2, 2, simplex, conforming > > >::value) {
+      if (polOrder == 1) {
+        if (type.compare("L2") == 0)
+          return {1.15e-01, 3.04e-02, 7.51e-03, 1.86e-03};
+        else if (type.compare("H1_semi") == 0)
+          return {3.79e-01, 1.90e-01, 9.38e-02, 4.67e-02};
+        else
+          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+      } else if (polOrder == 2) {
+        if (type.compare("L2") == 0)
+          return {1.25e-02, 1.42e-03, 1.69e-04, 2.08e-05};
+        else if (type.compare("H1_semi") == 0)
+          return {7.84e-02, 2.01e-02, 5.02e-03, 1.26e-03};
+        else
+          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+      } else
+        DUNE_THROW(NotImplemented, "Please record the expected results for this polOrder!");
+//    } else if (std::is_same< TestCaseType, EllipticTestCaseType::LocalThermalBlock< Dune::ALUConformGrid< 2, 2 > > >::value) {
+//      if (polOrder == 1) {
+//        if (type.compare("L2") == 0)
+//          return {5.85e-02, 2.00e-02, 5.55e-03, 1.30e-03};
+//        else if (type.compare("H1_semi") == 0)
+//          return {4.33e-01, 2.94e-01, 1.51e-01, 6.55e-02};
+//        else
+//          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+//      } else if (polOrder == 2) {
+//        if (type.compare("L2") == 0)
+//          return {1.19e-02, 2.12e-03, 3.90e-04, 7.77e-05};
+//        else if (type.compare("H1_semi") == 0)
+//          return {1.70e-01, 5.97e-02, 1.95e-02, 6.05e-03};
+//        else
+//          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+//      } else
+//        DUNE_THROW(NotImplemented, "Please record the expected results for this polOrder!");
+//    } else if (std::is_same< TestCaseType, EllipticTestCaseType::ER07< Dune::ALUConformGrid< 2, 2 > > >::value) {
+//      if (polOrder == 1) {
+//        if (type.compare("L2") == 0)
+//          return {6.10e-02, 1.66e-02, 4.23e-03};
+//        else if (type.compare("H1_semi") == 0)
+//          return {2.99e-01, 1.47e-01, 7.26e-02};
+//        else
+//          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+//      } else if (polOrder == 2) {
+//        if (type.compare("L2") == 0)
+//          return {6.43e-03, 8.24e-04, 1.05e-04};
+//        else if (type.compare("H1_semi") == 0)
+//          return {5.41e-02, 1.42e-02, 3.56e-03};
+//        else
+//          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+//      } else
+//        DUNE_THROW(NotImplemented, "Please record the expected results for this polOrder!");
+//    } else if (std::is_same< TestCaseType, EllipticTestCaseType::MixedBoundaryTypes< Dune::ALUConformGrid< 2, 2 > > >::value) {
+//      if (polOrder == 1) {
+//        if (type.compare("L2") == 0)
+//          return {4.03e-02, 1.13e-02, 2.84e-03, 6.34e-04};
+//        else if (type.compare("H1_semi") == 0)
+//          return {2.70e-01, 1.40e-01, 6.88e-02, 3.09e-02};
+//        else
+//          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+//      } else if (polOrder == 2) {
+//        if (type.compare("L2") == 0)
+//          return {3.59e-03, 6.26e-04, 1.22e-04, 2.69e-05};
+//        else if (type.compare("H1_semi") == 0)
+//          return {4.82e-02, 1.80e-02, 7.20e-03, 2.86e-03};
+//        else
+//          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+//      } else
+//        DUNE_THROW(NotImplemented, "Please record the expected results for this polOrder!");
+//    } else if (std::is_same< TestCaseType, EllipticTestCaseType::Spe10Model1< Dune::ALUConformGrid< 2, 2 > > >::value) {
+//      if (polOrder == 1) {
+//        if (type.compare("L2") == 0)
+//          return {7.23e-02, 2.60e-02};
+//        else if (type.compare("H1_semi") == 0)
+//          return {5.29e-01, 3.49e-01};
+//        else
+//          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+//      } else if (polOrder == 2) {
+//        if (type.compare("L2") == 0)
+//          return {2.09e-02, 3.75e-03};
+//        else if (type.compare("H1_semi") == 0)
+//          return {2.57e-01, 8.48e-02};
+//        else
+//          DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+//      } else
+//        DUNE_THROW(NotImplemented, "Please record the expected results for this polOrder!");
+    } else
+      DUNE_THROW(NotImplemented, "Please record the expected results for this TestCaseType/GridType combination!");
+  } // ... expected_results(...)
+
+private:
+  virtual double compute_norm_(const GridViewType& grid_view,
+                               const FunctionType& function,
+                               const std::string type) const
+  {
+    using namespace GDT;
+    if (type == "L2") {
+      return Products::L2< GridViewType >(grid_view).induced_norm(function);
+    } else if (type == "H1_semi") {
+      return Products::H1SemiGeneric< GridViewType >(grid_view).induced_norm(function);
+    } else
+      DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Wrong type '" << type << "' requested!");
+  } // ... compute_norm_(...)
+}; // class EocStudySWIPDG
+
+
+} // namespace Tests
+} // namespace LinearElliptic
+} // namespace HDD
+} // namespace Dune
+
+#endif // DUNE_HDD_TEST_LINEARELLIPTIC_SWIPDG_HH
