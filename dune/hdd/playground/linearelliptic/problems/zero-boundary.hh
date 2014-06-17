461a5f2fe9ddd3e6dc099492d4e3dee8d6000575
Felix Schindler <felix@schindlerfamily.de>
2014-06-17 23:32:16 +0200

    [...problems.zero-boundary] base on Problems::Default
    Upon updating to expose the shared_ptrs I ran into very strange segfaults
    that I could not resolve. Using the updated interface I can now base this
    class on Problems::Default and get let the base class handle the memory.

--- dune/hdd/playground/linearelliptic/problems/zero-boundary.hh
+++ dune/hdd/playground/linearelliptic/problems/zero-boundary.hh
@@ -10,7 +10,7 @@
 
 #include <dune/pymor/functions/default.hh>
 
-#include <dune/hdd/linearelliptic/problems/interfaces.hh>
+#include <dune/hdd/linearelliptic/problems/default.hh>
 
 namespace Dune {
 namespace HDD {
@@ -20,13 +20,13 @@ namespace Problems {
 
 template< class ProblemImp >
 class ZeroBoundary
-  : public ProblemInterface< typename ProblemImp::EntityType
-                           , typename ProblemImp::DomainFieldType, ProblemImp::dimDomain
-                           , typename ProblemImp::RangeFieldType, ProblemImp::dimRange >
+  : public Problems::Default< typename ProblemImp::EntityType
+                            , typename ProblemImp::DomainFieldType, ProblemImp::dimDomain
+                            , typename ProblemImp::RangeFieldType, ProblemImp::dimRange >
 {
-  typedef ProblemInterface< typename ProblemImp::EntityType
-                          , typename ProblemImp::DomainFieldType, ProblemImp::dimDomain
-                          , typename ProblemImp::RangeFieldType, ProblemImp::dimRange > BaseType;
+  typedef Problems::Default< typename ProblemImp::EntityType
+                           , typename ProblemImp::DomainFieldType, ProblemImp::dimDomain
+                           , typename ProblemImp::RangeFieldType, ProblemImp::dimRange > BaseType;
 public:
   using typename BaseType::EntityType;
   using typename BaseType::DomainFieldType;
@@ -34,9 +34,6 @@ public:
   static const unsigned int dimDomain = BaseType::dimDomain;
   static const unsigned int dimRange = BaseType::dimRange;
 
-  using typename BaseType::DiffusionFactorType;
-  using typename BaseType::DiffusionTensorType;
-  using typename BaseType::FunctionType;
 private:
   typedef Stuff::Functions::Constant< EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange >
     ConstantFunctionType;
@@ -55,38 +52,12 @@ public:
   }
 
   ZeroBoundary(const ProblemImp& problem)
-    : problem_(problem)
-    , zero_(new ConstantFunctionType(0))
+    : BaseType(problem.diffusion_factor(),
+               problem.diffusion_tensor(),
+               problem.force(),
+               std::make_shared< NonparametricFunctionType >(new ConstantFunctionType(0)),
+               std::make_shared< NonparametricFunctionType >(new ConstantFunctionType(0)))
   {}
-
-  virtual const DiffusionFactorType& diffusion_factor() const DS_OVERRIDE DS_FINAL
-  {
-    return problem_.diffusion_factor();
-  }
-
-  virtual const DiffusionTensorType& diffusion_tensor() const DS_OVERRIDE DS_FINAL
-  {
-    return problem_.diffusion_tensor();
-  }
-
-  virtual const FunctionType& force() const DS_OVERRIDE DS_FINAL
-  {
-    return problem_.force();
-  }
-
-  virtual const FunctionType& dirichlet() const DS_OVERRIDE DS_FINAL
-  {
-    return zero_;
-  }
-
-  virtual const FunctionType& neumann() const DS_OVERRIDE DS_FINAL
-  {
-    return zero_;
-  }
-
-private:
-  const ProblemImp& problem_;
-  const NonparametricFunctionType zero_;
 }; // class ZeroBoundary
 
 
