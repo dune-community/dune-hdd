180161899b9d6c6231bbd8ece35dd1f251520029
Felix Schindler <felix.schindler@wwu.de>
2014-08-20 19:44:29 +0200

    [test.OS2014] updated test structure and added nonparametric test

--- test/OS2014.cc
+++ test/OS2014.cc
@@ -6,8 +6,15 @@
 #define DUNE_STUFF_FUNCTIONS_DISABLE_CHECKS 1
 #define DUNE_HDD_LINEARELLIPTIC_TESTCASES_BASE_DISABLE_WARNING 1
 
+#ifndef DUNE_STUFF_TEST_MAIN_CATCH_EXCEPTIONS
+# define DUNE_STUFF_TEST_MAIN_CATCH_EXCEPTIONS 1
+#endif
+#ifndef DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING
+# define DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING 1
+#endif
+
 // This one has to come first (includes the config.h)!
-#include <dune/stuff/test/test_common.hh>
+#include <dune/stuff/test/main.hh>
 
 #include <sstream>
 
@@ -28,11 +35,6 @@
 #include "linearelliptic-swipdg.hh"
 #include "linearelliptic-block-swipdg.hh"
 
-// change this to toggle output
-std::ostream& test_out = std::cout;
-//std::ostream& test_out = DSC_LOG.devnull();
-
-
 using namespace Dune;
 using namespace HDD;
 
@@ -40,54 +42,66 @@ using namespace HDD;
 class OS2014_nonparametric_convergence_study
   : public ::testing::Test
 {
+protected:
+  static const GDT::ChooseSpaceBackend  space_backend =
+#if HAVE_DUNE_FEM
+                                                        GDT::ChooseSpaceBackend::fem;
+#else
+# error This test requires dune-fem!
+#endif
+  static const Stuff::LA::ChooseBackend la_backend    =
+#if HAVE_EIGEN
+                                                        Stuff::LA::ChooseBackend::eigen_sparse;
+#else
+                                                        Stuff::LA::default_sparse_backend;
+#endif
+
+  typedef ALUGrid< 2, 2, simplex, conforming > GridType;
+
+  typedef LinearElliptic::TestCases::ESV2007< GridType >           TestCaseType;
+  typedef LinearElliptic::TestCases::ESV2007Multiscale< GridType > BlockTestCaseType;
+
+  typedef LinearElliptic::Tests::EocStudySWIPDG< TestCaseType, 1, space_backend, la_backend > EocStudyType;
+  typedef LinearElliptic::Tests::EocStudyBlockSWIPDG< BlockTestCaseType, 1, la_backend >      BlockEocStudyType;
+
   template< class StudyType >
   static void check_for_success(const StudyType& study, std::map< std::string, std::vector< double > >& errors_map)
   {
-    for (const auto& norm : study.provided_norms())
-    {
+    for (const auto& norm : study.provided_norms()) {
       const auto errors = errors_map[norm];
       const auto expected_results = study.expected_results(norm);
       assert(expected_results.size() <= errors.size());
       for (size_t ii = 0; ii < errors.size(); ++ii)
-        EXPECT_LE(errors[ii], expected_results[ii]) << "          'norm' = " << norm << ", level = " << ii;
+        EXPECT_LE(errors[ii], expected_results[ii])
+            << "          norm = " << norm << ", level = " << ii;
     }
   } // ... check_for_success(...)
 
-  static const GDT::ChooseSpaceBackend  space_backend = GDT::ChooseSpaceBackend::fem;
-  static const Stuff::LA::ChooseBackend la_backend    = Stuff::LA::ChooseBackend::eigen_sparse;
-
-  typedef ALUGrid< 2, 2, simplex, conforming > GridType;
-
-  typedef LinearElliptic::TestCases::OS2014< GridType >           TestCaseType;
-//  typedef LinearElliptic::TestCases::ESV2007Multiscale< GridType > BlockTestCaseType;
-
-  typedef LinearElliptic::Tests::EocStudySWIPDG< TestCaseType, 1, space_backend, la_backend > EocStudyType;
-//  typedef LinearElliptic::Tests::EocStudyBlockSWIPDG< BlockTestCaseType, 1, la_backend >      BlockEocStudyType;
-
-protected:
-  void SWIPDG_fine_triangulation() const
+  template< class StudyType, class TestType >
+  void run_study(const TestType& test_case) const
   {
-    const TestCaseType test_case;
-    test_case.print_header(test_out);
-    test_out << std::endl;
-    EocStudyType eoc_study(test_case);
-    auto errors = eoc_study.run(false, test_out);
+    StudyType eoc_study(test_case);
+    auto errors = eoc_study.run(false, DSC_LOG_INFO);
     check_for_success(eoc_study, errors);
-  } // ... SWIPDG_fine_triangulation(...)
-
-//  bool BlockSWIPDG_coarse_triangulation(const std::string partitioning) const
-//  {
-//    const BlockTestCaseType test_case(partitioning);
-//    BlockEocStudyType eoc_study(test_case);
-//    auto errors = eoc_study.run(false, test_out);
-//    return check_for_success(eoc_study, errors);
-//  } // ... BlockSWIPDG_coarse_triangulation(...)
+  } // ... run_study(...)
 }; // class OS2014_nonparametric_convergence_study
 
 
 TEST_F(OS2014_nonparametric_convergence_study, SWIPDG_fine_triangulation) {
-  SWIPDG_fine_triangulation();
+  TestCaseType test_case;
+  test_case.print_header(DSC_LOG_INFO);
+  DSC_LOG_INFO << std::endl;
+  run_study< EocStudyType >(test_case);
 }
-
-
-#include <dune/stuff/test/test_main.cxx>
+TEST_F(OS2014_nonparametric_convergence_study, Block_SWIPDG_01_subdomain) {
+  run_study< BlockEocStudyType >(BlockTestCaseType("[1 1 1]"));
+}
+TEST_F(OS2014_nonparametric_convergence_study, Block_SWIPDG_04_subdomain) {
+  run_study< BlockEocStudyType >(BlockTestCaseType("[2 2 1]"));
+}
+TEST_F(OS2014_nonparametric_convergence_study, Block_SWIPDG_16_subdomain) {
+  run_study< BlockEocStudyType >(BlockTestCaseType("[4 4 1]"));
+}
+TEST_F(OS2014_nonparametric_convergence_study, Block_SWIPDG_64_subdomain) {
+  run_study< BlockEocStudyType >(BlockTestCaseType("[8 8 1]"));
+}
