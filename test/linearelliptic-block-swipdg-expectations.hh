907da9e3e508264fbc91b9728491cd6cf3df909f
Felix Schindler <felix.schindler@wwu.de>
2014-08-27 19:35:40 +0200

    [test.linearelliptic-block-swipdg-expectations] updated error message

--- test/linearelliptic-block-swipdg-expectations.hh
+++ test/linearelliptic-block-swipdg-expectations.hh
@@ -57,9 +57,9 @@ public:
                << "TestCaseType: " << Stuff::Common::Typename< TestCaseType >::value() << "\n"
                << "polOrder: " << polOrder << "\n"
                << "type: " << type << "\n"
-               << "Please put an appropriate specialiaztion of EocStudyBlockSWIPDGExpectations for this TestCaseType\n"
-               << "in a separate file, see examples below, or add a\n"
-               << "  'template class EocStudyBlockSWIPDGExpectations< TestCaseType, " << polOrder << ", true >\n"
+               << "Please put an appropriate specialiaztion of EocStudyBlockSWIPDGExpectations for this TestCaseType "
+               << "in a separate object file (see examples below) or add a\n"
+               << "  'template class EocStudyBlockSWIPDGExpectations< TestCaseType, " << polOrder << ", true >'\n"
                << "declaration in the appropriate object file!");
     return {};
   } // ... results(...)
@@ -70,7 +70,7 @@ public:
 
 
 extern template class EocStudyBlockSWIPDGExpectations< TestCases::ESV2007Multiscale< ALUGrid< 2, 2, simplex, conforming > >, 1, true >;
-extern template class EocStudyBlockSWIPDGExpectations< TestCases::OS2014Multiscale< ALUGrid< 2, 2, simplex, conforming > >, 0, true >;
+extern template class EocStudyBlockSWIPDGExpectations< TestCases::OS2014Multiscale< ALUGrid< 2, 2, simplex, conforming > >, 1, true >;
 
 
 #endif // HAVE_ALUGRID
