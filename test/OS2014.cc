05df67b3e79da1c73ebacffe1839e15030f3568c
Felix Schindler <felix.schindler@wwu.de>
2014-08-27 19:38:39 +0200

    [test.OS2014] added first draft for a parametric testcase

--- test/OS2014.cc
+++ test/OS2014.cc
@@ -30,6 +30,7 @@
 
 using namespace Dune;
 using namespace HDD;
+using Parameter = Pymor::Parameter;
 
 
 static const GDT::ChooseSpaceBackend  space_backend =
@@ -50,11 +51,14 @@ typedef ALUGrid< 2, 2, simplex, conforming > GridType;
 
 typedef LinearElliptic::TestCases::ESV2007< GridType >           NonparametricTestCaseType;
 typedef LinearElliptic::TestCases::ESV2007Multiscale< GridType > NonparametricBlockTestCaseType;
+typedef LinearElliptic::TestCases::OS2014Multiscale< GridType >  ParametricBlockTestCaseType;
 
 typedef LinearElliptic::Tests::EocStudySWIPDG< NonparametricTestCaseType, 1, space_backend, la_backend >
                                                                  NonparametricEocStudyType;
 typedef LinearElliptic::Tests::EocStudyBlockSWIPDG< NonparametricBlockTestCaseType, 1, la_backend >
                                                                  NonparametricBlockEocStudyType;
+typedef LinearElliptic::Tests::EocStudyBlockSWIPDG< ParametricBlockTestCaseType, 1, la_backend >
+                                                                 ParametricBlockEocStudyType;
 
 
 void nonparametric_block_convergence_study(const std::string& partitioning)
@@ -65,6 +69,38 @@ void nonparametric_block_convergence_study(const std::string& partitioning)
                                             "eff_OS2014"});
   check_for_success(eoc_study, eoc_study.run(false, DSC_LOG_INFO));
 } // ... nonparametric_block_convergence_study(...)
+
+
+template< class PT >
+void print_parameter_information(const PT& parametric_test_case)
+{
+  const auto& parameters = parametric_test_case.parameters();
+  const auto& parametric_problem = parametric_test_case.problem();
+  for (auto parameter : parameters)
+    EXPECT_EQ(parameter.second.type(), parametric_problem.parameter_type())
+        << "          id: " << parameter.first << ", parameter: " << parameter.second;
+  const auto& diffusion_factor = *parametric_problem.diffusion_factor();
+  const auto& diffusion_tensor = *parametric_problem.diffusion_tensor();
+  const auto& force = *parametric_problem.force();
+  const auto& dirichlet = *parametric_problem.dirichlet();
+  const auto& neumann = *parametric_problem.neumann();
+  EXPECT_TRUE(diffusion_factor.parametric());
+  EXPECT_FALSE(diffusion_tensor.parametric());
+  EXPECT_FALSE(force.parametric());
+  EXPECT_FALSE(dirichlet.parametric());
+  EXPECT_FALSE(neumann.parametric());
+  DSC_LOG_INFO << "| mu            = " << parameters.at("mu") << "\n"
+               << "| mu_bar        = " << parameters.at("mu_bar") << "\n"
+               << "| mu_hat        = " << parameters.at("mu_hat") << "\n"
+               << "| mu_minimizing = " << parameters.at("mu_minimizing") << "\n";
+  const double alpha = diffusion_factor.alpha(parameters.at("mu"), parameters.at("mu_hat"));
+  const double gamma = diffusion_factor.gamma(parameters.at("mu"), parameters.at("mu_hat"));
+  DSC_LOG_INFO << "| alpha(mu, mu_hat)^-1/2    = " << std::setprecision(2) << std::scientific
+                                                   << 1.0/std::sqrt(alpha) << "\n"
+               << "| gamma_tilde(mu, mu_hat)^2 = " << std::setprecision(2) << std::scientific
+                                                   << std::max(std::sqrt(gamma), 1.0/std::sqrt(alpha)) << "\n"
+               << "+==================================================================+\n";
+} // ... print_parameter_information(...)
 
 
 TEST(OS2014_nonparametric_convergence_study, SWIPDG_fine_triangulation)
@@ -95,6 +131,22 @@ TEST(OS2014_nonparametric_convergence_study, Block_SWIPDG_16_subdomain) {
 }
 TEST(OS2014_nonparametric_convergence_study, Block_SWIPDG_64_subdomain) {
   nonparametric_block_convergence_study("[8 8 1]");
+}
+
+TEST(OS2014_parametric_convergence_study, Block_SWIPDG_01_subdomain)
+{
+  const ParametricBlockTestCaseType test_case({{"mu_hat", Parameter("mu", 1)},
+                                               {"mu_bar", Parameter("mu", 1)},
+                                               {"mu", Parameter("mu", 1)},
+                                               {"mu_minimizing", Parameter("mu", 1)}},
+                                              "[1 1 1]");
+  test_case.print_header(DSC_LOG_INFO);
+  print_parameter_information(test_case);
+  DSC_LOG_INFO << std::endl;
+  ParametricBlockEocStudyType eoc_study(test_case,
+                                        {"energy_mu", "eta_NC_OS2014", "eta_R_OS2014", "eta_DF_OS2014", "eta_OS2014",
+                                         "eff_OS2014_mu"});
+  check_for_success(eoc_study, eoc_study.run(false, DSC_LOG_INFO));
 }
 
 
