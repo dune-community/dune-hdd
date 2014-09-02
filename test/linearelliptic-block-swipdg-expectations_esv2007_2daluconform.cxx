b01d135050c71770ae68863ad53091390d08c7cc
Felix Schindler <felix.schindler@wwu.de>
2014-09-02 11:52:30 +0200

    [test.linearelliptic-block-swipdg-expectations] updated ESV2007

--- test/linearelliptic-block-swipdg-expectations_esv2007_2daluconform.cxx
+++ test/linearelliptic-block-swipdg-expectations_esv2007_2daluconform.cxx
@@ -30,17 +30,17 @@ public:
   {
     if (test_case.partitioning() == "[1 1 1]") {
       if (type == "energy")
-        return {3.29e-01, 1.63e-01, 8.05e-02, 4.02e-02};
+        return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
       else if (type == "eta_NC_OS2014")
-        return {1.67e-01, 7.90e-02, 3.92e-02, 1.96e-02};
+        return {1.66e-01, 7.89e-02, 3.91e-02, 1.95e-02};
       else if (type == "eta_R_OS2014")
-        return {5.80e-01, 2.91e-01, 1.46e-01, 7.28e-02};
+        return {5.79e-01, 2.90e-01, 1.45e-01, 7.27e-02};
       else if (type == "eta_DF_OS2014")
-        return {3.56e-01, 1.77e-01, 8.74e-02, 4.36e-02};
+        return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
       else if (type == "eta_OS2014")
-        return {1.11e+00, 5.46e-01, 2.73e-01, 1.37e-01};
+        return {1.10e+00, 5.45e-01, 2.72e-01, 1.36e-01};
       else if (type == "eff_OS2014")
-        return {3.36, 3.38, 3.39, 3.40};
+        return {3.35e+00, 3.37e+00, 3.38e+00, 3.39e+00};
       else
         DUNE_THROW(Stuff::Exceptions::test_results_missing, type);
     } else if (test_case.partitioning() == "[2 2 1]") {
@@ -75,17 +75,17 @@ public:
         DUNE_THROW(Stuff::Exceptions::test_results_missing, type);
     } else if (test_case.partitioning() == "[8 8 1]") {
       if (type == "energy")
-        return {3.29e-01, 1.63e-01, 8.05e-02, 4.02e-02};
+        return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
       else if (type == "eta_NC_OS2014")
-        return {1.67e-01, 7.90e-02, 3.92e-02, 1.96e-02};
+        return {1.66e-01, 7.89e-02, 3.91e-02, 1.95e-02};
       else if (type == "eta_R_OS2014")
-        return {7.24e-02, 3.64e-02, 1.83e-02, 9.10e-03};
+        return {7.23e-02, 3.63e-02, 1.82e-02, 9.09e-03};
       else if (type == "eta_DF_OS2014")
-        return {3.56e-01, 1.77e-01, 8.74e-02, 4.36e-02};
+        return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
       else if (type == "eta_OS2014")
-        return {1.11e+00, 5.46e-01, 2.73e-01, 1.37e-01};
+        return {5.93e-01, 2.91e-01, 1.45e-01, 7.21e-02};
       else if (type == "eff_OS2014")
-        return {1.82, 1.81, 1.81, 1.81};
+        return {1.81, 1.80, 1.80, 1.80};
       else
         DUNE_THROW(Stuff::Exceptions::test_results_missing, type);
     } else
