9ac91943bfbaef672a4483b65db1b9e749b57f16
Felix Schindler <felix.schindler@wwu.de>
2014-09-08 12:07:57 +0200

    [test.linearelliptic-block-swipdg-expectations] added expected results

--- test/linearelliptic-block-swipdg-expectations_esv2007_2daluconform.cxx
+++ test/linearelliptic-block-swipdg-expectations_esv2007_2daluconform.cxx
@@ -30,22 +30,36 @@ public:
   static std::vector< double > results(const TestCaseType& test_case, const std::string type)
   {
     if (test_case.partitioning() == "[1 1 1]") {
-      if (type == "energy")
+      if (type == "L2")
+        return {1.83e-02, 4.53e-03, 1.12e-03, 2.78e-04};
+      else if (type == "H1_semi")
+        return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
+      else if (type == "energy")
         return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
       else if (type == "eta_NC_OS2014")
         return {1.66e-01, 7.89e-02, 3.91e-02, 1.95e-02};
       else if (type == "eta_R_OS2014")
         return {5.79e-01, 2.90e-01, 1.45e-01, 7.27e-02};
+      else if (type == "eta_DF_OS2014_*")
+        return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
       else if (type == "eta_DF_OS2014")
         return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
       else if (type == "eta_OS2014")
         return {1.10e+00, 5.45e-01, 2.72e-01, 1.36e-01};
+      else if (type == "eta_OS2014_*")
+        return {1.10e+00, 5.45e-01, 2.72e-01, 1.36e-01};
       else if (type == "eff_OS2014")
+        return {3.35e+00, 3.37e+00, 3.38e+00, 3.39e+00};
+      else if (type == "eff_OS2014_*")
         return {3.35e+00, 3.37e+00, 3.38e+00, 3.39e+00};
       else
         EXPECT_TRUE(false) << "test results missing for type: " << type;
     } else if (test_case.partitioning() == "[2 2 1]") {
-      if (type == "energy")
+      if (type == "L2")
+        return {1.83e-02, 4.53e-03, 1.12e-03, 2.78e-04};
+      else if (type == "H1_semi")
+        return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
+      else if (type == "energy")
         return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
       else if (type == "eta_NC_OS2014")
         return {1.66e-01, 7.89e-02, 3.91e-02, 1.95e-02};
@@ -53,14 +67,24 @@ public:
         return {2.89e-01, 1.45e-01, 7.27e-02, 3.63e-02};
       else if (type == "eta_DF_OS2014")
         return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
+      else if (type == "eta_DF_OS2014_*")
+        return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
       else if (type == "eta_OS2014")
         return {8.10e-01, 4.00e-01, 1.99e-01, 9.94e-02};
+      else if (type == "eta_OS2014_*")
+        return {8.10e-01, 4.00e-01, 1.99e-01, 9.94e-02};
       else if (type == "eff_OS2014")
+        return {2.47, 2.47, 2.48, 2.48};
+      else if (type == "eff_OS2014_*")
         return {2.47, 2.47, 2.48, 2.48};
       else
         EXPECT_TRUE(false) << "test results missing for type: " << type;
     } else if (test_case.partitioning() == "[4 4 1]") {
-      if (type == "energy")
+      if (type == "L2")
+        return {1.83e-02, 4.53e-03, 1.12e-03, 2.78e-04};
+      else if (type == "H1_semi")
+        return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
+      else if (type == "energy")
         return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
       else if (type == "eta_NC_OS2014")
         return {1.66e-01, 7.89e-02, 3.91e-02, 1.95e-02};
@@ -68,14 +92,24 @@ public:
         return {1.45e-01, 7.26e-02, 3.63e-02, 1.82e-02};
       else if (type == "eta_DF_OS2014")
         return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
+      else if (type == "eta_DF_OS2014_*")
+        return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
       else if (type == "eta_OS2014")
         return {6.65e-01, 3.27e-01, 1.63e-01, 8.12e-02};
+      else if (type == "eta_OS2014_*")
+        return {6.65e-01, 3.27e-01, 1.63e-01, 8.12e-02};
       else if (type == "eff_OS2014")
+        return {2.03, 2.02, 2.02, 2.03};
+      else if (type == "eff_OS2014_*")
         return {2.03, 2.02, 2.02, 2.03};
       else
         EXPECT_TRUE(false) << "test results missing for type: " << type;
     } else if (test_case.partitioning() == "[8 8 1]") {
-      if (type == "energy")
+      if (type == "L2")
+        return {1.83e-02, 4.53e-03, 1.12e-03, 2.78e-04};
+      else if (type == "H1_semi")
+        return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
+      else if (type == "energy")
         return {3.28e-01, 1.62e-01, 8.04e-02, 4.01e-02};
       else if (type == "eta_NC_OS2014")
         return {1.66e-01, 7.89e-02, 3.91e-02, 1.95e-02};
@@ -83,9 +117,15 @@ public:
         return {7.23e-02, 3.63e-02, 1.82e-02, 9.09e-03};
       else if (type == "eta_DF_OS2014")
         return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
+      else if (type == "eta_DF_OS2014_*")
+        return {3.55e-01, 1.76e-01, 8.73e-02, 4.35e-02};
       else if (type == "eta_OS2014")
         return {5.93e-01, 2.91e-01, 1.45e-01, 7.21e-02};
+      else if (type == "eta_OS2014_*")
+        return {5.93e-01, 2.91e-01, 1.45e-01, 7.21e-02};
       else if (type == "eff_OS2014")
+        return {1.81, 1.80, 1.80, 1.80};
+      else if (type == "eff_OS2014_*")
         return {1.81, 1.80, 1.80, 1.80};
       else
         EXPECT_TRUE(false) << "test results missing for type: " << type;
