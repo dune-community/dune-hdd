61029318d8a2ed05d3302723a5cde2677a833531
Felix Schindler <felix.schindler@wwu.de>
2014-09-08 12:34:39 +0200

    [test.OS2014-FVCA7-Poster] update
    * use new testing framework
    * only print energy norm and estimator
    * add proper guards

--- test/OS2014-FVCA7-poster.cc
+++ test/OS2014-FVCA7-poster.cc
@@ -5,32 +5,30 @@
 
 #define DUNE_STUFF_FUNCTIONS_DISABLE_CHECKS 1
 #define DUNE_HDD_LINEARELLIPTIC_TESTCASES_BASE_DISABLE_WARNING 1
+#ifndef DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING
+# define DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING 1
+#endif
 
 // This one has to come first (includes the config.h)!
 #include <dune/stuff/test/main.hxx>
 
 #include <sstream>
 
-#if HAVE_ALUGRID_SERIAL_H || HAVE_ALUGRID_PARALLEL_H
-# define ENABLE_ALUGRID 1
-# include <dune/grid/alugrid.hh>
-#else
-# error This test requires ALUGrid!
-#endif
+#if HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM && HAVE_DUNE_ISTL && HAVE_ALUGRID
 
-#include <dune/stuff/common/exceptions.hh>
+# include <dune/stuff/common/disable_warnings.hh>
+#   include <dune/grid/alugrid.hh>
+# include <dune/stuff/common/reenable_warnings.hh>
 
-#include <dune/stuff/common/print.hh>
-#include <dune/stuff/common/float_cmp.hh>
+# include <dune/stuff/common/exceptions.hh>
 
-#include <dune/hdd/playground/linearelliptic/testcases/ESV2007.hh>
+# include <dune/stuff/common/print.hh>
+# include <dune/stuff/common/float_cmp.hh>
 
-#include "linearelliptic-swipdg.hh"
-#include "linearelliptic-block-swipdg.hh"
+# include <dune/hdd/playground/linearelliptic/testcases/ESV2007.hh>
 
-// change this to toggle output
-std::ostream& test_out = std::cout;
-//std::ostream& test_out = DSC_LOG.devnull();
+# include "linearelliptic-swipdg.hh"
+# include "linearelliptic-block-swipdg.hh"
 
 
 using namespace Dune;
@@ -40,37 +38,6 @@ using namespace HDD;
 class OS2014_FVCA7_Poster_convergence_study
   : public ::testing::Test
 {
-  static std::vector< double > truncate_vector_(const std::vector< double >& in, const size_t size)
-  {
-    assert(size <= in.size());
-    if (size == in.size())
-      return in;
-    else {
-      std::vector< double > ret(size);
-      for (size_t ii = 0; ii < size; ++ii)
-        ret[ii] = in[ii];
-      return ret;
-    }
-  } // ... truncate_vector(...)
-
-  template< class StudyType >
-  static bool check_for_success_(const StudyType& study, std::map< std::string, std::vector< double > >& errors)
-  {
-    size_t failures = 0;
-    std::stringstream ss;
-    ss << "\n";
-    for (const auto& norm : study.provided_norms())
-      if (!Dune::Stuff::Common::FloatCmp::lt(errors[norm],
-                                             truncate_vector_(study.expected_results(norm), errors[norm].size()))) {
-        ++failures;
-        Dune::Stuff::Common::print(errors[norm],                 "errors           (" + norm + ")", ss);
-        Dune::Stuff::Common::print(study.expected_results(norm), "expected results (" + norm + ")", ss);
-      }
-    if (failures)
-      DUNE_THROW(errors_are_not_as_expected, ss.str());
-    return true;
-  } // ... check_for_success_(...)
-
   typedef ALUGrid< 2, 2, simplex, conforming > GridType;
 
   typedef LinearElliptic::TestCases::ESV2007< GridType >           TestCaseType;
@@ -79,45 +46,57 @@ class OS2014_FVCA7_Poster_convergence_study
   static const GDT::ChooseSpaceBackend  space_backend = GDT::ChooseSpaceBackend::fem;
   static const Stuff::LA::ChooseBackend la_backend    = Stuff::LA::ChooseBackend::istl_sparse;
 
-  typedef LinearElliptic::Tests::EocStudySWIPDG< TestCaseType, 1, space_backend, la_backend > EocStudyType;
-  typedef LinearElliptic::Tests::EocStudyBlockSWIPDG< BlockTestCaseType, 1, la_backend >      BlockEocStudyType;
+  typedef LinearElliptic::Tests::SWIPDGStudy< TestCaseType, 1, space_backend, la_backend > EocStudyType;
+  typedef LinearElliptic::Tests::BlockSWIPDGStudy< BlockTestCaseType, 1, la_backend >      BlockEocStudyType;
 
 protected:
-  bool ESV2007_fine_triangulation() const
+  void ESV2007_fine_triangulation() const
   {
     const TestCaseType test_case;
-    test_case.print_header(test_out);
-    test_out << std::endl;
-    EocStudyType eoc_study(test_case);
-    auto errors = eoc_study.run(false, test_out);
-    return check_for_success_(eoc_study, errors);
+    test_case.print_header(DSC_LOG_INFO);
+    DSC_LOG_INFO << std::endl;
+    EocStudyType eoc_study(test_case, {"energy", "eta_ESV2007"});
+    Stuff::Test::check_eoc_study_for_success(eoc_study, eoc_study.run_eoc(DSC_LOG_INFO));
   } // ... ESV2007_fine_triangulation(...)
 
-  bool BlockSWIPDG_coarse_triangulation(const std::string partitioning) const
+  void BlockSWIPDG_coarse_triangulation(const std::string partitioning) const
   {
     const BlockTestCaseType test_case(partitioning);
-    BlockEocStudyType eoc_study(test_case);
-    auto errors = eoc_study.run(false, test_out);
-    return check_for_success_(eoc_study, errors);
+    BlockEocStudyType eoc_study(test_case, {"energy", "eta_OS2014"});
+    Stuff::Test::check_eoc_study_for_success(eoc_study, eoc_study.run_eoc(DSC_LOG_INFO));
   } // ... BlockSWIPDG_coarse_triangulation(...)
 }; // class OS2014_FVCA7_Poster_convergence_study
 
 
 TEST_F(OS2014_FVCA7_Poster_convergence_study, ESV2007_fine_triangulation) {
-  EXPECT_TRUE(ESV2007_fine_triangulation());
+  ESV2007_fine_triangulation();
 }
 TEST_F(OS2014_FVCA7_Poster_convergence_study, BlockSWIPDG_01_subdomain) {
-  EXPECT_TRUE(BlockSWIPDG_coarse_triangulation("[1 1 1]"));
+  BlockSWIPDG_coarse_triangulation("[1 1 1]");
 }
 TEST_F(OS2014_FVCA7_Poster_convergence_study, BlockSWIPDG_04_subdomain) {
-  EXPECT_TRUE(BlockSWIPDG_coarse_triangulation("[2 2 1]"));
+  BlockSWIPDG_coarse_triangulation("[2 2 1]");
 }
 TEST_F(OS2014_FVCA7_Poster_convergence_study, BlockSWIPDG_16_subdomain) {
-  EXPECT_TRUE(BlockSWIPDG_coarse_triangulation("[4 4 1]"));
+  BlockSWIPDG_coarse_triangulation("[4 4 1]");
 }
 TEST_F(OS2014_FVCA7_Poster_convergence_study, BlockSWIPDG_64_subdomain) {
-  EXPECT_TRUE(BlockSWIPDG_coarse_triangulation("[8 8 1]"));
+  BlockSWIPDG_coarse_triangulation("[8 8 1]");
 }
 
 
-#include <dune/stuff/test/test_main.cxx>
+extern template class LinearElliptic::Tests::BlockSWIPDGStudyExpectations
+    < LinearElliptic::TestCases::ESV2007Multiscale< ALUGrid< 2, 2, simplex, conforming > >, 1 >;
+
+
+#else // HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM && HAVE_DUNE_ISTL && HAVE_ALUGRID
+
+
+TEST_F(DISABLED_OS2014_FVCA7_Poster_convergence_study, ESV2007_fine_triangulation) {}
+TEST_F(DISABLED_OS2014_FVCA7_Poster_convergence_study, BlockSWIPDG_01_subdomain) {}
+TEST_F(DISABLED_OS2014_FVCA7_Poster_convergence_study, BlockSWIPDG_04_subdomain) {}
+TEST_F(DISABLED_OS2014_FVCA7_Poster_convergence_study, BlockSWIPDG_16_subdomain) {}
+TEST_F(DISABLED_OS2014_FVCA7_Poster_convergence_study, BlockSWIPDG_64_subdomain) {}
+
+
+#endif // HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM && HAVE_DUNE_ISTL && HAVE_ALUGRID
