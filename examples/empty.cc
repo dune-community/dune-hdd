ed17a265323d0f71bb846bf62ac297c32159317c
Felix Schindler <felix.schindler@wwu.de>
2014-09-10 15:23:15 +0200

    [examples.empty] added

---
+++ examples/empty.cc
@@ -0,0 +1,17 @@
+// This file is part of the dune-hdd project:
+//   http://users.dune-project.org/projects/dune-hdd
+// Copyright holders: Felix Albrecht
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+/**
+  * This file is intended to serve as a starting point for quick tests.
+  */
+
+#include "config.h"
+
+
+int main(int /*argc*/, char** /*argv*/)
+{
+
+  return 0;
+} // ... main(...)
