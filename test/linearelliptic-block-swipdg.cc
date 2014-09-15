6a529f55825e6afeb31f505f2a7fc28fea73b7d8
Felix Schindler <felix.schindler@wwu.de>
2014-09-15 16:57:39 +0200

    [test.linearelliptic-block-swipdg] update

--- test/linearelliptic-block-swipdg.cc
+++ test/linearelliptic-block-swipdg.cc
@@ -3,27 +3,28 @@
 // Copyright holders: Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-#define DUNE_STUFF_FUNCTIONS_DISABLE_CHECKS 1
 #ifndef DUNE_STUFF_TEST_MAIN_CATCH_EXCEPTIONS
-# define DUNE_STUFF_TEST_MAIN_CATCH_EXCEPTIONS 1
+# define DUNE_STUFF_TEST_MAIN_CATCH_EXCEPTIONS
 #endif
 #ifndef DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING
-# define DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING 1
+# define DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING
 #endif
+
+#define DUNE_HDD_LINEARELLIPTIC_TESTCASES_BASE_DISABLE_WARNING
 
 // This one has to come first (includes the config.h)!
 #include <dune/stuff/test/main.hxx>
 
-#if HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM && HAVE_ALUGRID
+#if HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM
 
 # include <dune/stuff/common/disable_warnings.hh>
+# if HAVE_ALUGRID
 #   include <dune/grid/alugrid.hh>
+# endif
+# include <dune/grid/sgrid.hh>
 # include <dune/stuff/common/reenable_warnings.hh>
 
 # include <dune/stuff/common/exceptions.hh>
-
-# include <dune/stuff/common/print.hh>
-# include <dune/stuff/common/float_cmp.hh>
 
 # include <dune/hdd/playground/linearelliptic/testcases/ESV2007.hh>
 
@@ -33,75 +34,108 @@ using namespace Dune;
 using namespace HDD;
 
 
-typedef ALUGrid< 2, 2, simplex, conforming > AluConform2dGridType;
-
-typedef testing::Types< LinearElliptic::TestCases::ESV2007Multiscale< AluConform2dGridType >
-                      > AluConform2dTestCases;
-
-
 template< class TestCaseType >
-struct linearelliptic_SWIPDG_discretization
+struct linearelliptic_BlockSWIPDG_discretization
   : public ::testing::Test
 {
   template< Stuff::LA::ChooseBackend la_backend >
   static void eoc_study(const std::string partitioning = "[1 1 1]")
   {
-    const TestCaseType test_case(partitioning);
+    const TestCaseType test_case(partitioning, 2);
     test_case.print_header(DSC_LOG_INFO);
     DSC_LOG_INFO << std::endl;
     LinearElliptic::Tests::BlockSWIPDGStudy< TestCaseType, 1, la_backend > eoc_study(test_case);
     Stuff::Test::check_eoc_study_for_success(eoc_study, eoc_study.run_eoc(DSC_LOG_INFO));
   } // ... eoc_study()
-}; // linearelliptic_SWIPDG_discretization
+}; // linearelliptic_BlockSWIPDG_discretization
 
 
-TYPED_TEST_CASE(linearelliptic_SWIPDG_discretization, AluConform2dTestCases);
+typedef SGrid< 2, 2 > SGridType;
+# if HAVE_ALUGRID
+typedef ALUGrid< 2, 2, simplex, conforming > AluConform2dGridType;
+# endif // HAVE_ALUGRID
 
-# if HAVE_DUNE_ISTL
-TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_1_subdomain) {
-  this->template eoc_study< Stuff::LA::ChooseBackend::istl_sparse >("[1 1 1]");
-}
-TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_4_subdomain) {
-  this->template eoc_study< Stuff::LA::ChooseBackend::istl_sparse >("[2 2 1]");
-}
-TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_16_subdomain) {
-  this->template eoc_study< Stuff::LA::ChooseBackend::istl_sparse >("[4 4 1]");
-}
-TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_64_subdomain) {
-  this->template eoc_study< Stuff::LA::ChooseBackend::istl_sparse >("[8 8 1]");
-}
-# elif HAVE_EIGEN // HAVE_DUNE_ISTL
-TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_1_subdomain) {
+typedef testing::Types< LinearElliptic::TestCases::ESV2007Multiscale< SGridType >
+# if HAVE_ALUGRID
+                      , LinearElliptic::TestCases::ESV2007Multiscale< AluConform2dGridType >
+# endif
+                      > TestCases;
+TYPED_TEST_CASE(linearelliptic_BlockSWIPDG_discretization, TestCases);
+
+
+# if HAVE_EIGEN
+TYPED_TEST(linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_1_subdomain) {
   this->template eoc_study< Stuff::LA::ChooseBackend::eigen_sparse >("[1 1 1]");
 }
-TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_4_subdomain) {
+TYPED_TEST(linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_4_subdomains) {
   this->template eoc_study< Stuff::LA::ChooseBackend::eigen_sparse >("[2 2 1]");
 }
-TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_16_subdomain) {
+TYPED_TEST(linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_16_subdomains) {
   this->template eoc_study< Stuff::LA::ChooseBackend::eigen_sparse >("[4 4 1]");
 }
-TYPED_TEST(linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_64_subdomain) {
+TYPED_TEST(linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_64_subdomains) {
   this->template eoc_study< Stuff::LA::ChooseBackend::eigen_sparse >("[8 8 1]");
 }
-#else // HAVE_EIGEN // HAVE_DUNE_ISTL
-TEST(DISABLED_linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_1_subdomain) {}
-TEST(DISABLED_linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_4_subdomain) {}
-TEST(DISABLED_linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_16_subdomain) {}
-TEST(DISABLED_linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_64_subdomain) {}
-# endif // HAVE_EIGEN // HAVE_DUNE_ISTL
+# else // HAVE_EIGEN
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_1_subdomain) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_4_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_16_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_64_subdomains) {}
+# endif // HAVE_EIGEN
 
+# if HAVE_DUNE_ISTL
+TYPED_TEST(linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_1_subdomain) {
+  this->template eoc_study< Stuff::LA::ChooseBackend::istl_sparse >("[1 1 1]");
+}
+TYPED_TEST(linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_4_subdomains) {
+  this->template eoc_study< Stuff::LA::ChooseBackend::istl_sparse >("[2 2 1]");
+}
+TYPED_TEST(linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_16_subdomains) {
+  this->template eoc_study< Stuff::LA::ChooseBackend::istl_sparse >("[4 4 1]");
+}
+TYPED_TEST(linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_64_subdomains) {
+  this->template eoc_study< Stuff::LA::ChooseBackend::istl_sparse >("[8 8 1]");
+}
+# else // HAVE_DUNE_ISTL
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_1_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_4_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_16_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_64_subdomains) {}
+# endif // HAVE_DUNE_ISTL
+
+
+namespace Dune {
+namespace HDD {
+namespace LinearElliptic {
+namespace Tests {
+
+//extern template class LinearElliptic::Tests::BlockSWIPDGStudyExpectations
+//    < LinearElliptic::TestCases::ESV2007Multiscale< SGridType >, 1 >;
+
+# if HAVE_ALUGRID
 
 extern template class LinearElliptic::Tests::BlockSWIPDGStudyExpectations
-    < LinearElliptic::TestCases::ESV2007Multiscale< ALUGrid< 2, 2, simplex, conforming > >, 1 >;
+    < LinearElliptic::TestCases::ESV2007Multiscale< AluConform2dGridType >, 1 >;
+
+# endif // HAVE_ALUGRID
+
+} // namespace Tests
+} // namespace LinearElliptic
+} // namespace HDD
+} // namespace Dune
+
+#else // HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM
 
 
-#else // HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM && HAVE_ALUGRID
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_1_subdomain) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_4_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_16_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_eigen_on_64_subdomains) {}
+
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_1_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_4_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_16_subdomains) {}
+TEST(DISABLED_linearelliptic_BlockSWIPDG_discretization, eoc_study_using_istl_on_64_subdomains) {}
 
 
-TEST(DISABLED_linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_1_subdomain) {}
-TEST(DISABLED_linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_4_subdomain) {}
-TEST(DISABLED_linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_16_subdomain) {}
-TEST(DISABLED_linearelliptic_SWIPDG_discretization, eoc_study_using_istl_and_64_subdomain) {}
-
-
-#endif // HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM && HAVE_ALUGRID
+#endif // HAVE_DUNE_GRID_MULTISCALE && HAVE_DUNE_FEM
