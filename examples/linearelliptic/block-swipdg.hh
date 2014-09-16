48560b121db4ec042a09799255519dbd58086658
Felix Schindler <felix.schindler@wwu.de>
2014-09-16 15:09:52 +0200

    merged discreteproblem from playground

--- examples/linearelliptic/block-swipdg.hh
+++ examples/linearelliptic/block-swipdg.hh
@@ -10,7 +10,7 @@
 
 #include <dune/stuff/common/memory.hh>
 
-#include <dune/hdd/playground/linearelliptic/discreteproblem.hh>
+#include <dune/hdd/linearelliptic/discreteproblem.hh>
 #include <dune/hdd/playground/linearelliptic/discretizations/block-swipdg.hh>
 
 template< class GridImp >
