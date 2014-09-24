c88c92d0dd1fe499e3c1ad62518fdb5cc2ed8e1e
Felix Schindler <felix.schindler@wwu.de>
2014-09-24 21:45:46 +0200

    [examples.linearelliptic.OS2014] added python bindings

---
+++ examples/linearelliptic/OS2014_bindings_generator.hh
@@ -0,0 +1,18 @@
+// This file is part of the dune-hdd project:
+//   http://users.dune-project.org/projects/dune-hdd
+// Copyright holders: Felix Albrecht
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_HDD_EXAMPLES_LINEARELLIPTIC_OS2014_BINDINGS_GENERATOR_HH
+#define DUNE_HDD_EXAMPLES_LINEARELLIPTIC_OS2014_BINDINGS_GENERATOR_HH
+
+#include "config.h"
+
+#include "OS2014.hh"
+
+#include <dune/stuff/common/disable_warnings.hh>
+# if HAVE_ALUGRID
+#   include <dune/grid/alugrid.hh>
+# endif
+
+#endif // DUNE_HDD_EXAMPLES_LINEARELLIPTIC_OS2014_BINDINGS_GENERATOR_HH
---
+++ examples/linearelliptic/OS2014_bindings_generator.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python
+# This file is part of the dune-hdd project:
+#   http://users.dune-project.org/projects/dune-hdd
+# Copyright holders: Felix Albrecht
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+import sys
+from pybindgen import param, retval
+
+from dune.pymor.core import prepare_python_bindings, inject_lib_dune_pymor, finalize_python_bindings
+from dune.pymor.discretizations import inject_StationaryDiscretizationImplementation
+from dune.pymor.discretizations import inject_StationaryMultiscaleDiscretizationImplementation
+
+
+def inject_Example(module, exceptions, interfaces, CONFIG_H):
+    '''injects the user code into the module'''
+    # first the discretization
+    GridType = 'Dune::ALUGrid< 2, 2, Dune::simplex, Dune::conforming >'
+    #GridType = 'Dune::SGrid< 2, 2 >'
+    RangeFieldType = 'double'
+    dimRange = '1'
+    polOrder = '1'
+    la_backend = 'Dune::Stuff::LA::ChooseBackend::eigen_sparse'
+    if la_backend is 'Dune::Stuff::LA::ChooseBackend::eigen_sparse':
+        MatrixType = 'Dune::Stuff::LA::EigenRowMajorSparseMatrix< ' + RangeFieldType + ' >'
+        VectorType = 'Dune::Stuff::LA::EigenDenseVector< ' + RangeFieldType + ' >'
+    OperatorType = 'Dune::Pymor::Operators::LinearAffinelyDecomposedContainerBased< ' + MatrixType + ', ' + VectorType + ' >'
+    ProductType = OperatorType
+    FunctionalType = 'Dune::Pymor::Functionals::LinearAffinelyDecomposedVectorBased< ' + VectorType + ' >'
+    DiscretizationName = 'Dune::HDD::LinearElliptic::Discretizations::BlockSWIPDG'
+    DiscretizationFullName = (DiscretizationName + '< '
+                              + GridType + ', '
+                              + RangeFieldType + ', '
+                              + dimRange + ', ' + polOrder + ', '
+                              + la_backend + '>')
+    discretization = inject_StationaryMultiscaleDiscretizationImplementation(
+        module, exceptions, interfaces, CONFIG_H,
+        DiscretizationName,
+        Traits={'VectorType': VectorType,
+                'OperatorType': OperatorType,
+                'FunctionalType': FunctionalType,
+                'ProductType': ProductType},
+        template_parameters=[GridType, RangeFieldType, dimRange, polOrder, la_backend])
+    # then add the example
+    Example = module.add_class('OS2014Spe10Model1Example',
+                               template_parameters=[GridType],
+                               custom_name='OS2014Spe10Model1Example')
+    Example.add_constructor([param('const std::string', 'partitioning'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'num_refinements'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'info_log_levels'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'debug_log_levels'),
+                             param('const bool', 'enable_warnings'),
+                             param('const bool', 'enable_colors'),
+                             param('const std::string', 'info_color'),
+                             param('const std::string', 'debug_color'),
+                             param('const std::string', 'warn_color')],
+                            throw=[exceptions['Exception']])
+    Example.add_constructor([param('const std::string', 'partitioning'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'num_refinements'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'info_log_levels'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'debug_log_levels'),
+                             param('const bool', 'enable_warnings'),
+                             param('const bool', 'enable_colors'),
+                             param('const std::string', 'info_color'),
+                             param('const std::string', 'debug_color')],
+                            throw=[exceptions['Exception']])
+    Example.add_constructor([param('const std::string', 'partitioning'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'num_refinements'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'info_log_levels'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'debug_log_levels'),
+                             param('const bool', 'enable_warnings'),
+                             param('const bool', 'enable_colors'),
+                             param('const std::string', 'info_color')],
+                            throw=[exceptions['Exception']])
+    Example.add_constructor([param('const std::string', 'partitioning'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'num_refinements'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'info_log_levels'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'debug_log_levels'),
+                             param('const bool', 'enable_warnings'),
+                             param('const bool', 'enable_colors')],
+                            throw=[exceptions['Exception']])
+    Example.add_constructor([param('const std::string', 'partitioning'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'num_refinements'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'info_log_levels'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'debug_log_levels'),
+                             param('const bool', 'enable_warnings')],
+                            throw=[exceptions['Exception']])
+    Example.add_constructor([param('const std::string', 'partitioning'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'num_refinements'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'info_log_levels'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'debug_log_levels')],
+                            throw=[exceptions['Exception']])
+    Example.add_constructor([param('const std::string', 'partitioning'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'num_refinements'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'info_log_levels')],
+                            throw=[exceptions['Exception']])
+    Example.add_constructor([param('const std::string', 'partitioning'),
+                             param('const ' + CONFIG_H['DUNE_STUFF_SSIZE_T'], 'num_refinements')],
+                            throw=[exceptions['Exception']])
+    Example.add_constructor([], throw=[exceptions['Exception']])
+    Example.add_method('discretization_and_return_ptr',
+                       retval(DiscretizationFullName + ' *', caller_owns_return=True),
+                       [], is_const=True, throw=[exceptions['Exception']],
+                       custom_name='discretization')
+
+if __name__ == '__main__':
+    # prepare the module
+    module, pybindgen_filename, config_h_filename = prepare_python_bindings(sys.argv[1:])
+    # add all of libdunepymor
+    module, exceptions, interfaces, CONFIG_H = inject_lib_dune_pymor(module, config_h_filename)
+    # add example user code (see above)
+    inject_Example(module, exceptions, interfaces, CONFIG_H)
+    # and finally write the pybindgen .cc file
+    finalize_python_bindings(module, pybindgen_filename)
