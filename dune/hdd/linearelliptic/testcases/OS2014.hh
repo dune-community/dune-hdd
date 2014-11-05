65c605ccc5023ea81552028e0c26ab065a2b3ef0
Felix Schindler <felix.schindler@wwu.de>
2014-11-05 01:47:49 +0100

    [linearelliptic.testcases.os2014] add FiveSpot

--- dune/hdd/linearelliptic/testcases/OS2014.hh
+++ dune/hdd/linearelliptic/testcases/OS2014.hh
@@ -140,6 +140,120 @@ protected:
 }; // class ParametricConvergenceBase
 
 
+template< class GridType >
+class FiveSpotBase
+{
+  static_assert(GridType::dimension == 2, "This test case is only available in 2d!");
+public:
+  typedef typename GridType::template Codim< 0 >::Entity EntityType;
+  typedef typename GridType::ctype                       DomainFieldType;
+  static const unsigned int                              dimDomain = GridType::dimension;
+  typedef double            RangeFieldType;
+  static const unsigned int dimRange = 1;
+public:
+  typedef Problems::OS2014::FiveSpot
+      < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange > ProblemType;
+  typedef Stuff::Functions::Constant
+      < EntityType, DomainFieldType, dimDomain, RangeFieldType, dimRange > ExactSolutionType;
+  typedef typename ProblemType::FunctionType::NonparametricType            FunctionType;
+
+  typedef std::map< std::string, Pymor::ParameterType > ParameterTypesMapType;
+  typedef std::map< std::string, Pymor::Parameter >     ParametersMapType;
+
+protected:
+  static const size_t default_num_refinements_ = 3;
+
+  static int initial_refinements()
+  {
+    int ret = 1;
+#if HAVE_ALUGRID
+    if (std::is_same< GridType, ALUConformGrid< 2, 2 > >::value
+        || std::is_same< GridType, ALUGrid< 2, 2, simplex, conforming > >::value)
+      ret += 1;
+#endif // HAVE_ALUGRID
+    return ret;
+  } // ... initial_refinements()
+
+  static ParametersMapType add_parameter_range(const ParametersMapType& parameters)
+  {
+    ParametersMapType ret = parameters;
+    ret["parameter_range_min"] = Pymor::Parameter("mu", 0.1);
+    ret["parameter_range_max"] = Pymor::Parameter("mu", 0.9);
+    return ret;
+  } // ... add_parameter_range(...)
+
+public:
+  static ParameterTypesMapType required_parameters()
+  {
+    return ParameterTypesMapType({{"mu",     Pymor::ParameterType("mu", 1)},
+                                  {"mu_bar", Pymor::ParameterType("mu", 1)},
+                                  {"mu_hat", Pymor::ParameterType("mu", 1)}});
+  }
+
+  FiveSpotBase(const ParametersMapType parameters)
+    : parameters_(add_parameter_range(parameters))
+    , boundary_info_cfg_(Stuff::Grid::BoundaryInfoConfigs::AllDirichlet::default_config())
+    , problem_(2)
+    , exact_solution_(0)
+  {}
+
+  void print_header(std::ostream& out = std::cout) const
+  {
+    out << "+===============================================================================+\n"
+        << "|+=============================================================================+|\n"
+        << "||  Testcase OS2014: (parametric) five spot study                              ||\n"
+        << "||  (see Ohlberger, Schindler, 2014)                                           ||\n"
+        << "|+-----------------------------------------------------------------------------+|\n"
+        << "||  domain = [-1, 1] x [-1, 1]                                                 ||\n"
+        << "||  diffusion = 1                                                              ||\n"
+        << "||              - mu      *exp(-((x + 1/2)^2/(2.0*0.1^2)))                     ||\n"
+        << "||              - (1 - mu)*exp(-((x - 1/2)^2/(2.0*0.1^2)))                     ||\n"
+        << "||  force     =   exp(-((x + 3/4)^2/(2.0*0.1^2)) - ((y + 3/4)^2/(2.0*0.1^2)))  ||\n"
+        << "||              + exp(-((x - 3/4)^2/(2.0*0.1^2)) - ((y - 3/4)^2/(2.0*0.1^2)))  ||\n"
+        << "||              + exp(-((x - 3/4)^2/(2.0*0.1^2)) - ((y + 3/4)^2/(2.0*0.1^2)))  ||\n"
+        << "||              + exp(-((x + 3/4)^2/(2.0*0.1^2)) - ((y - 3/4)^2/(2.0*0.1^2)))  ||\n"
+        << "||              - exp(-(        x^2/(2.0*0.1^2)) - (        y^2/(2.0*0.1^2)))  ||\n"
+        << "||  dirichlet = 0                                                              ||\n"
+        << "||  reference solution: discrete solution on finest grid                       ||\n"
+        << "||  parameter: mu in [0.1, 0.9]                                                ||\n"
+        << "|+=============================================================================+|\n"
+        << "+===============================================================================+" << std::endl;
+  } // ... print_header(...)
+
+  const Stuff::Common::Configuration& boundary_info() const
+  {
+    return boundary_info_cfg_;
+  }
+
+  const ProblemType& problem() const
+  {
+    return problem_;
+  }
+
+  bool provides_exact_solution() const
+  {
+    return false;
+  }
+
+  const ExactSolutionType& exact_solution() const
+  {
+    DUNE_THROW(Stuff::Exceptions::you_are_using_this_wrong,
+               "Do not call exact_solution() if provides_exact_solution() is false!");
+  }
+
+  const ParametersMapType& parameters() const
+  {
+    return parameters_;
+  }
+
+protected:
+  const ParametersMapType parameters_;
+  const Stuff::Common::Configuration boundary_info_cfg_;
+  const ProblemType problem_;
+  const ExactSolutionType exact_solution_;
+}; // class FiveSpotBase
+
+
 } // namespace internal
 
 
@@ -213,6 +327,42 @@ public:
     this->inherit_parameter_type(this->problem_, "problem");
   }
 }; // class ParametricBlockConvergence
+
+
+template< class GridType >
+class FiveSpotBlock
+  : public internal::FiveSpotBase< GridType >
+  , public MultiscaleCubeBase< GridType >
+{
+  typedef internal::FiveSpotBase< GridType > FiveSpotBaseType;
+  typedef MultiscaleCubeBase< GridType >     TestCaseBaseType;
+
+  static Stuff::Common::Configuration initial_grid_cfg(const std::string num_partitions)
+  {
+    Stuff::Common::Configuration grid_cfg = Stuff::Grid::Providers::Cube< GridType >::default_config();
+    grid_cfg["lower_left"] = "-1";
+    grid_cfg["upper_right"] = "1";
+    grid_cfg["num_elements"] = "4";
+    grid_cfg["num_partitions"] = num_partitions;
+    return grid_cfg;
+  } // ... initial_grid_cfg(...)
+
+public:
+  typedef typename TestCaseBaseType::ParametersMapType ParametersMapType;
+
+  using FiveSpotBaseType::required_parameters;
+  using FiveSpotBaseType::parameters;
+
+  FiveSpotBlock(const ParametersMapType parameters,
+                const std::string num_partitions = "[1 1 1]",
+                const size_t num_refinements = FiveSpotBaseType::default_num_refinements_)
+    : FiveSpotBaseType(parameters)
+    , TestCaseBaseType(initial_grid_cfg(num_partitions), FiveSpotBaseType::initial_refinements(), num_refinements)
+  {
+    this->check_parameters(FiveSpotBaseType::required_parameters(), parameters);
+    this->inherit_parameter_type(this->problem_, "problem");
+  }
+}; // class FiveSpotBlock
 
 
 #endif // HAVE_DUNE_GRID_MULTISCALE
