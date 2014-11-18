7e4027a1c7b2c8ea9d26971582074b84060b2b9a
Felix Schindler <felix.schindler@wwu.de>
2014-11-18 20:16:19 +0100

    [linearelliptic.problems.OS2014] add LocalThermalblock

--- dune/hdd/linearelliptic/problems/OS2014.hh
+++ dune/hdd/linearelliptic/problems/OS2014.hh
@@ -8,6 +8,7 @@
 
 #include <memory>
 
+#include <dune/stuff/playground/functions/indicator.hh>
 #include <dune/stuff/functions/constant.hh>
 #include <dune/stuff/functions/expression.hh>
 #include <dune/stuff/functions/ESV2007.hh>
@@ -210,6 +211,89 @@ public:
     return BaseType::BaseType::static_id() + ".OS2014.fivespot";
   }
 }; // class FiveSpot< ..., 2, ..., 1 >
+
+
+
+template< class E, class D, int d, class R, int r = 1 >
+class LocalThermalblock
+  : public ProblemInterface< E, D, d, R, r >
+{
+  LocalThermalblock() { static_assert(AlwaysFalse< E >::value, "Not available for dimRange > 1!"); }
+};
+
+
+template< class EntityImp, class DomainFieldImp, class RangeFieldImp >
+class LocalThermalblock< EntityImp, DomainFieldImp, 2, RangeFieldImp, 1 >
+  : public Default< EntityImp, DomainFieldImp, 2, RangeFieldImp, 1 >
+{
+  typedef Default< EntityImp, DomainFieldImp, 2, RangeFieldImp, 1 >  BaseType;
+  typedef LocalThermalblock< EntityImp, DomainFieldImp, 2, RangeFieldImp, 1 > ThisType;
+
+  typedef Stuff::Functions::Indicator< EntityImp, DomainFieldImp, 2, RangeFieldImp, 1 >   IndicatorFunctionType;
+  typedef Stuff::Functions::Constant< EntityImp, DomainFieldImp, 2, RangeFieldImp, 1 >    ConstantScalarFunctionType;
+  typedef Stuff::Functions::Constant< EntityImp, DomainFieldImp, 2, RangeFieldImp, 2, 2 > ConstantMatrixFunctionType;
+  typedef Pymor::Functions::NonparametricDefault< EntityImp, DomainFieldImp, 2, RangeFieldImp, 1 >
+      ParametricScalarFunctionType;
+  typedef Pymor::Functions::NonparametricDefault< EntityImp, DomainFieldImp, 2, RangeFieldImp, 2, 2 >
+      ParametricMatrixFunctionType;
+  typedef Pymor::Functions::AffinelyDecomposableDefault< EntityImp, DomainFieldImp, 2, RangeFieldImp, 1 >
+      DefaultParametricFunctionType;
+
+  static std::shared_ptr< DefaultParametricFunctionType > create_diffusion_factor()
+  {
+    DSC::Configuration affine_part_cfg;
+    affine_part_cfg["name"] = "omega_0";
+    affine_part_cfg["0.domain"] = "[0 0.333; 0 0.166]";
+    affine_part_cfg["0.value"] = "1";
+    affine_part_cfg["1.domain"] = "[0 0.166; 0.166 0.333]";
+    affine_part_cfg["1.value"] = "1";
+    affine_part_cfg["2.domain"] = "[0 0.333; 0.333 1]";
+    affine_part_cfg["2.value"] = "1";
+    affine_part_cfg["3.domain"] = "[0.333 1; 0 1]";
+    affine_part_cfg["3.value"] = "1";
+    DSC::Configuration local_block_cfg;
+    local_block_cfg["name"] = "omega_1";
+    local_block_cfg["0.domain"] = "[0.166 0.333; 0.166 0.333]";
+    local_block_cfg["0.value"] = "1";
+    auto ret = std::make_shared< DefaultParametricFunctionType >(IndicatorFunctionType::create(affine_part_cfg));
+    ret->register_component(IndicatorFunctionType::create(local_block_cfg),
+                            new Pymor::ParameterFunctional("mu", 1, "mu"));
+    return ret;
+  } // ... create_diffusion_factor(...)
+
+public:
+  static const bool available = true;
+
+  static std::string static_id()
+  {
+    return BaseType::BaseType::static_id() + ".OS2014.localthermalblock";
+  }
+
+  static Stuff::Common::Configuration default_config(const std::string /*sub_name*/ = "")
+  {
+    return Stuff::Common::Configuration();
+  }
+
+  static std::unique_ptr< ThisType > create(const Stuff::Common::Configuration /*config*/ = default_config(),
+                                            const std::string /*sub_name*/ = static_id())
+  {
+    return Stuff::Common::make_unique< ThisType >();
+  }
+
+  LocalThermalblock()
+    : BaseType(create_diffusion_factor(),
+               std::make_shared< ParametricMatrixFunctionType >(new ConstantMatrixFunctionType(
+                  DS::Functions::internal::unit_matrix< RangeFieldImp, 2 >(), "diffusion_tensor")),
+               std::make_shared< ParametricScalarFunctionType >(new ConstantScalarFunctionType(1, "force")),
+               std::make_shared< ParametricScalarFunctionType >(new ConstantScalarFunctionType(0, "dirichlet")),
+               std::make_shared< ParametricScalarFunctionType >(new ConstantScalarFunctionType(0, "neumann")))
+  {}
+
+  virtual std::string type() const override
+  {
+    return BaseType::BaseType::static_id() + ".OS2014.localthermalblock";
+  }
+}; // class LocalThermalblock< ..., 2, ..., 1 >
 
 
 } // namespace OS2014
