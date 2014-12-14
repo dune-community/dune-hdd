7442ff66f9556f636a84b673ce9a605ce668ab2e
Felix Schindler <felix.schindler@wwu.de>
2014-12-14 18:07:40 +0100

    [test...block-swipdg-expectations.spe10...] update expected results
    Due to changes in the testcase and the test.

--- test/linearelliptic-block-swipdg-expectations_spe10_2daluconform.cxx
+++ test/linearelliptic-block-swipdg-expectations_spe10_2daluconform.cxx
@@ -32,66 +32,44 @@ public:
     const auto mu     = test_case.parameters().at("mu");
     const auto mu_bar = test_case.parameters().at("mu_bar");
     const auto mu_hat = test_case.parameters().at("mu_hat");
-    if (test_case.num_refinements() == 2 && test_case.partitioning() == "[10 2 1]") {
-      // fine grid sizes: 8000, 32000, 128000
-      // fine grid width: 8.84e-03, 4.42e-03, 2.21e-03
+    if (test_case.num_refinements() == 3 && test_case.partitioning() == "[25 5 1]_H_with_h") {
+      // fine grid sizes: 16000, 64000, 256000, 1024000
+      // fine grid width: 6.25e-03, 3.12e-03, 1.56e-03, 7.81e-04
       if (mu == 1 && mu_bar == 1 && mu_hat == 1) {
         if (type == "energy_mu")
-          return {9.24e-01, 5.21e-01, 2.51e-01};
+          return {7.49e-01, 4.52e-01, 2.58e-01, 1.26e-01}; // average order 0.86
         else if (type == "eta_NC_OS2014")
-          return {2.74e+00, 1.84e+00, 1.27e+00};
+          return {2.13e+00, 1.46e+00, 1.02e+00, 7.20e-01}; // average order 0.52
         else if (type == "eta_R_OS2014_*")
-          return {1.51e-09, 1.98e-08, 4.15e-08};
+          return {1.88e-09, 7.05e-10, 7.44e-11, 2.00e-10}; // average order 1.07 [1.41, 3.24, -1.43]
         else if (type == "eta_DF_OS2014_*")
-          return {1.22e+00, 7.62e-01, 4.81e-01};
+          return {9.66e-01, 6.05e-01, 3.85e-01, 2.49e-01}; // average order 0.65
         else if (type == "eta_OS2014_*")
-          return {3.97e+00, 2.60e+00, 1.75e+00};
+          return {3.10e+00, 2.07e+00, 1.40e+00,9.69e-01 }; // average order 0.56
         else if (type == "eff_OS2014_*_mu")
-          return {4.29e+00, 4.99e+00, 6.98e+00};
-        else
-          EXPECT_TRUE(false) << "test results missing for type: " << type;
-      } else
-        EXPECT_TRUE(false) << "test results missing for parameters: mu = " << mu << "\n"
-                           << "                                     mu_bar = " << mu_bar << "\n"
-                           << "                                     mu_hat = " << mu_hat;
-    } else if (test_case.num_refinements() == 2 && test_case.partitioning() == "[25 5 1]_H_with_h") {
-      // fine grid sizes: 8000, 32000, 128000
-      // fine grid width: 8.84e-03, 4.42e-03, 2.21e-03
-      if (mu == 1 && mu_bar == 1 && mu_hat == 1) {
-        if (type == "energy_mu")
-          return {9.24e-01, 5.21e-01, 2.51e-01}; // average order 0.94
-        else if (type == "eta_NC_OS2014")
-          return {2.74e+00, 1.84e+00, 1.27e+00}; // average order 0.56
-        else if (type == "eta_R_OS2014_*")
-          return {3.61e-09, 4.65e-10, 1.03e-10}; // average order 2.57
-        else if (type == "eta_DF_OS2014_*")
-          return {1.22e+00, 7.62e-01, 4.81e-01}; // average order 0.67
-        else if (type == "eta_OS2014_*")
-          return {3.97e+00, 2.60e+00, 1.75e+00}; // average order 0.59
-        else if (type == "eff_OS2014_*_mu")
-          return {4.29e+00, 4.99e+00, 6.98e+00};
+          return {4.14e+00, 4.58e+00, 5.44e+00, 7.70e+00};
         else
           EXPECT_TRUE(false) << "test results missing for type: " << type;
       } else if (mu == 1 && mu_bar == 1 && mu_hat == 0.1) {
         if (type == "eta_DF_OS2014_*")
-          return {1.89e+00, 1.16e+00, 7.26e-01}; // average order 0.69
+          return {1.50e+00, 9.28e-01, 5.80e-01, 3.68e-01}; // average order 0.68
         else if (type == "eta_OS2014_*")
-          return {4.63e+00, 3.00e+00, 2.00e+00}; // average order 0.61
+          return {3.63e+00, 2.39e+00, 1.60e+00, 1.09e+00}; // average order 0.58
         else if (type == "eff_OS2014_*_mu")
-          return {5.01e+00, 5.76e+00, 7.95e+00};
+          return {4.85e+00, 5.29e+00, 6.20e+00, 8.64e+00};
         else
           EXPECT_TRUE(false) << "test results missing for type: " << type;
       } else if (mu == 1 && mu_bar == 0.1 && mu_hat == 0.1) {
         if (type == "energy_mu_bar")
-          return {9.16e-01, 5.16e-01, 2.49e-01}; // average order 0.94
+          return {7.43e-01, 4.48e-01, 2.56e-01, 1.25e-01}; // average order 0.86
         else if (type == "eta_NC_OS2014")
-          return {2.01e+00, 1.25e+00, 8.06e-01}; // average order 0.66
+          return {1.51e+00, 9.64e-01, 6.29e-01, 4.17e-01}; // average order 0.62
         else if (type == "eta_DF_OS2014_*")
-          return {1.89e+00, 1.16e+00, 7.26e-01}; // average order 0.69
+          return {1.50e+00, 9.28e-01, 5.80e-01, 3.68e-01}; // average order 0.68
         else if (type == "eta_OS2014_*")
-          return {8.24e+00, 5.12e+00, 3.28e+00}; // average order 0.67
+          return {6.27e+00, 3.97e+00, 2.57e+00, 1.69e+00}; // average order 0.63
         else if (type == "eff_OS2014_*_mu_bar")
-          return {9.00e+00, 9.92e+00, 1.32e+01};
+          return {8.44e+00, 8.88e+00, 1.00e+01, 1.35e+01};
         else
           EXPECT_TRUE(false) << "test results missing for type: " << type;
       } else
