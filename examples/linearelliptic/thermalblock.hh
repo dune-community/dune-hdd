0e3151ecc94f748ceaad737bc2f63674fad2ed50
Felix Schindler <felix.schindler@wwu.de>
2015-05-01 14:05:51 +0200

    [examples.linearelliptic.thermalblock] reorder initialization
    This allows to adapt the grid before discretizing.

--- examples/linearelliptic/thermalblock.hh
+++ examples/linearelliptic/thermalblock.hh
@@ -8,11 +8,16 @@
 
 #include <boost/numeric/conversion/cast.hpp>
 
+#include <dune/common/parallel/mpihelper.hh>
+
+#if HAVE_ALUGRID
+# include <dune/grid/alugrid.hh>
+#endif
+
 #if HAVE_DUNE_FEM
 # include <dune/fem/misc/mpimanager.hh>
 #endif
 
-#include <dune/common/parallel/mpihelper.hh>
 
 #include <dune/stuff/common/exceptions.hh>
 #include <dune/stuff/common/logging.hh>
@@ -27,10 +32,14 @@
 namespace internal {
 
 
+template< class GridType >
 class Initializer
 {
+  typedef Dune::Stuff::Grid::Providers::Cube< GridType > GridProviderType;
+
 public:
-  Initializer(const DUNE_STUFF_SSIZE_T info_log_levels,
+  Initializer(const std::string& num_grid_elements,
+              const DUNE_STUFF_SSIZE_T info_log_levels,
               const DUNE_STUFF_SSIZE_T debug_log_levels,
               const bool enable_warnings,
               const bool enable_colors,
@@ -55,7 +64,18 @@ public:
                               debug_color,
                               warn_color);
     DSC::TimedLogger().get("cg.thermalblock.example").info() << "creating grid and problem... " << std::endl;
-  }
+    grid_provider_ = GridProviderType::create(GridProviderType::default_config().add(DSC::Configuration("num_elements",
+                                                                                                        num_grid_elements),
+                                                                                     "",
+                                                                                     true));
+#if HAVE_ALUGRID
+    if (std::is_same< GridType, Dune::ALUGrid< 2, 2, Dune::simplex, Dune::conforming > >::value)
+      grid_provider_->grid().globalRefine(1);
+#endif // HAVE_ALUGRID
+  } // Initializer(...)
+
+protected:
+  std::unique_ptr< GridProviderType > grid_provider_;
 }; // class Initializer
 
 
@@ -64,8 +84,9 @@ public:
 
 template< class GridImp, Dune::GDT::ChooseSpaceBackend space_backend, Dune::Stuff::LA::ChooseBackend la_backend >
 class CgExample
-  : internal::Initializer
+  : internal::Initializer< GridImp >
 {
+  typedef internal::Initializer< GridImp >               BaseType;
   typedef GridImp                                        GridType;
   typedef typename GridType::template Codim< 0 >::Entity EntityType;
   static const size_t                                    dimDomain = GridType::dimension;
@@ -74,7 +95,6 @@ class CgExample
   static const size_t                                    dimRange = 1;
   typedef Dune::HDD::LinearElliptic::Problems::Thermalblock
       < EntityType, DomainFieldType, dimDomain, RangeFieldType, 1 > ProblemType;
-  typedef Dune::Stuff::Grid::Providers::Cube< GridType >            GridProviderType;
 public:
   typedef Dune::HDD::LinearElliptic::Discretizations::CG< GridType, Dune::Stuff::Grid::ChooseLayer::leaf,
                                                           RangeFieldType, dimRange, 1,
@@ -89,21 +109,18 @@ public:
             const std::string info_color  = DSC::TimedLogging::default_info_color(),
             const std::string debug_color = DSC::TimedLogging::default_debug_color(),
             const std::string warn_color  = DSC::TimedLogging::default_warning_color())
-    : Initializer(info_log_levels,
-                  debug_log_levels,
-                  enable_warnings,
-                  enable_colors,
-                  info_color,
-                  debug_color,
-                  warn_color)
+    : BaseType(num_grid_elements,
+               info_log_levels,
+               debug_log_levels,
+               enable_warnings,
+               enable_colors,
+               info_color,
+               debug_color,
+               warn_color)
     , problem_(ProblemType::create(ProblemType::default_config().add(DSC::Configuration("diffusion_factor.num_elements",
                                                                                         num_blocks),
                                                                      "",
                                                                      true)))
-    , grid_provider_(GridProviderType::create(GridProviderType::default_config().add(DSC::Configuration("num_elements",
-                                                                                                        num_grid_elements),
-                                                                                     "",
-                                                                                     true)))
     , discretization_(*grid_provider_, Dune::Stuff::Grid::BoundaryInfoConfigs::AllDirichlet::default_config(), *problem_)
   {
     auto logger = DSC::TimedLogger().get("cg.thermalblock.example");
@@ -120,8 +137,8 @@ public:
   }
 
 private:
+  using BaseType::grid_provider_;
   std::unique_ptr< ProblemType > problem_;
-  std::unique_ptr< GridProviderType > grid_provider_;
   DiscretizationType discretization_;
 }; // class CgExample
 
