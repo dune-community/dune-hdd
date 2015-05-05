11dbd6d9734d94839290bf7f78069bea5f44b734
Rene Milk <rene.milk@uni-muenster.de>
2015-05-05 12:10:15 +0200

    [tests] fixes linear elliptic base for pdelab spaces

--- test/linearelliptic.hh
+++ test/linearelliptic.hh
@@ -13,16 +13,10 @@
 
 #include <dune/common/timer.hh>
 
-#if HAVE_DUNE_FEM
-# include <dune/fem/misc/gridwidth.hh>
-#else
-# error "We need dune-fem until we have a reliable and fast method to compute the grid width!"
-#endif
-
 #include <dune/stuff/common/memory.hh>
 #include <dune/stuff/common/convergence-study.hh>
 #include <dune/stuff/grid/layers.hh>
-
+#include <dune/stuff/grid/information.hh>
 #include <dune/gdt/discretefunction/default.hh>
 #include <dune/gdt/operators/prolongations.hh>
 
@@ -50,7 +44,7 @@ class EocStudyBase
   : public Stuff::Common::ConvergenceStudy
 {
   typedef Stuff::Common::ConvergenceStudy BaseType;
-protected:
+public:
 
   typedef DiscretizationImp DiscretizationType;
   typedef typename DiscretizationType::VectorType VectorType;
@@ -116,7 +110,7 @@ public:
         compute_reference_solution();
         assert(reference_discretization_);
         assert(reference_solution_vector_);
-        const ConstDiscreteFunctionType reference_solution(*(reference_discretization_->ansatz_space()),
+        const ConstDiscreteFunctionType reference_solution(reference_discretization_->ansatz_space(),
                                                            *reference_solution_vector_,
                                                            "reference solution");
         return compute_norm(test_case_.reference_grid_view(), reference_solution, type);
@@ -136,8 +130,8 @@ public:
   {
     assert(current_refinement_ <= num_refinements());
     const int level = test_case_.level_of(current_refinement_);
-    const auto grid_part = test_case_.template level< Stuff::Grid::ChoosePartView::part >(level);
-    return Fem::GridWidth::calcGridWidth(grid_part);
+    const auto grid_part = test_case_.template level< Stuff::Grid::ChoosePartView::view >(level);
+    return DSG::dimensions(grid_part).entity_width.max();
   } // ... current_grid_width(...)
 
   virtual double compute_on_current_refinement() override final
@@ -158,7 +152,7 @@ public:
           = Stuff::Common::make_unique< VectorType >(current_discretization_->create_vector());
       current_discretization_->solve(*current_solution_vector_on_level_);
       time_to_solution_ = timer.elapsed();
-      const ConstDiscreteFunctionType current_refinement_solution(*current_discretization_->ansatz_space(),
+      const ConstDiscreteFunctionType current_refinement_solution(current_discretization_->ansatz_space(),
                                                                   *current_solution_vector_on_level_,
                                                                   "solution on current level");
       // prolong to reference grid part
@@ -169,7 +163,7 @@ public:
       assert(reference_discretization_);
       if (!current_solution_vector_)
         current_solution_vector_ = Stuff::Common::make_unique< VectorType >(reference_discretization_->create_vector());
-      DiscreteFunctionType reference_refinement_solution(*(reference_discretization_->ansatz_space()),
+      DiscreteFunctionType reference_refinement_solution(reference_discretization_->ansatz_space(),
                                                          *current_solution_vector_,
                                                          "solution on reference grid part");
       prolongation_operator.apply(current_refinement_solution, reference_refinement_solution);
@@ -197,7 +191,7 @@ public:
       if (!reference_solution_computed_)
         compute_reference_solution();
       assert(reference_discretization_);
-      const ConstDiscreteFunctionType current_solution(*(reference_discretization_->ansatz_space()),
+      const ConstDiscreteFunctionType current_solution(reference_discretization_->ansatz_space(),
                                                        *current_solution_vector_,
                                                        "current solution");
       // compute error
@@ -208,7 +202,7 @@ public:
         compute_reference_solution();
         assert(reference_discretization_);
         assert(reference_solution_vector_);
-        const ConstDiscreteFunctionType reference_solution(*(reference_discretization_->ansatz_space()),
+        const ConstDiscreteFunctionType reference_solution(reference_discretization_->ansatz_space(),
                                                            *reference_solution_vector_,
                                                            "reference solution");
         return compute_norm(test_case_.reference_grid_view(), reference_solution- current_solution, type);
@@ -249,7 +243,7 @@ protected:
       if (!visualize_prefix_.empty()) {
         this->test_case_.problem().visualize(reference_discretization_->grid_view(),
                                              visualize_prefix_ + "_problem_reference");
-        ConstDiscreteFunctionType(*reference_discretization_->ansatz_space(),
+        ConstDiscreteFunctionType(reference_discretization_->ansatz_space(),
                                   *reference_solution_vector_,
                                   "reference solution").visualize(visualize_prefix_ + "_reference_solution");
       }
@@ -556,4 +550,5 @@ protected:
 } // namespace HDD
 } // namespace Dune
 
+namespace DHLT = Dune::HDD::LinearElliptic::Tests;
 #endif // DUNE_HDD_test_case_LINEARELLIPTIC_EOC_STUDY_HH
