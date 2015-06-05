479314add4d28de5b1776f2ddd8b64272e483ee0
Rene Milk <rene.milk@uni-muenster.de>
2015-06-05 15:09:28 +0200

    rewires mpi cg example to use thermalblock testcase/problem

--- examples/linearelliptic/mpi_cg_example.hh
+++ examples/linearelliptic/mpi_cg_example.hh
@@ -14,6 +14,8 @@
 #include <dune/hdd/linearelliptic/testcases/spe10model2.hh>
 #include <dune/hdd/linearelliptic/problems/spe10.hh>
 #include <dune/hdd/linearelliptic/testcases/spe10.hh>
+#include <dune/hdd/linearelliptic/problems/thermalblock.hh>
+#include <dune/hdd/linearelliptic/testcases/thermalblock.hh>
 
 class MpiCGExample
 {
@@ -21,29 +23,34 @@ public:
   typedef Dune::HDD::LinearElliptic::Discretizations::MpiCG
        DiscretizationType;
   typedef double RangeFieldType;
-  typedef typename Dune::SPGrid<RangeFieldType, 2> GridType;
+  static constexpr size_t dimDomain = 2;
+  typedef typename Dune::SPGrid<RangeFieldType, dimDomain> GridType;
   typedef Dune::Stuff::Grid::Providers::Cube< GridType > GridProviderType;
   static constexpr unsigned int dimRange = 1;
   typedef typename DSG::Entity<typename GridType::LeafGridView>::Type EntityType;
 
 //  typedef Dune::HDD::LinearElliptic::Problems::ESV2007
 //      < EntityType, RangeFieldType, dimRange, RangeFieldType, dimRange> ProblemType;
-  typedef Dune::HDD::LinearElliptic::TestCases::ESV2007
+//  typedef Dune::HDD::LinearElliptic::TestCases::ESV2007
+//      < GridType > TestcaseType;
+  typedef Dune::HDD::LinearElliptic::TestCases::Thermalblock
       < GridType > TestcaseType;
 
 //  typedef Dune::HDD::LinearElliptic::Problems::Spe10::Model1
 //      < EntityType, RangeFieldType, dimRange, RangeFieldType, dimRange> ProblemType;
 //  typedef Dune::HDD::LinearElliptic::TestCases::Spe10::ParametricModel1
 //      < GridType > TestcaseType;
-
+    typedef Dune::HDD::LinearElliptic::Problems::Thermalblock
+        < EntityType, RangeFieldType, dimRange, RangeFieldType, dimRange> ProblemType;
 
 public:
   MpiCGExample(const std::size_t num_refinements = 0)
-    : testcase_( 0 /*{{"mu",     Dune::Pymor::Parameter("mu", 1)},
+    : testcase_( TestcaseType::default_parameters(DSC::FieldVector<size_t, dimDomain>{{2u,2u}})
+                 /*{{"mu",     Dune::Pymor::Parameter("mu", 1)},
                   {"mu_hat", Dune::Pymor::Parameter("mu", 1)},
                   {"mu_bar", Dune::Pymor::Parameter("mu", 1)},
                   {"parameter_range_min", Dune::Pymor::Parameter("mu", 0.1)},
-                  {"parameter_range_max", Dune::Pymor::Parameter("mu", 1.0)}}*/, num_refinements)
+                  {"parameter_range_max", Dune::Pymor::Parameter("mu", 1.0)}}, num_refinements*/)
     , discretization_(testcase_,
                       testcase_.boundary_info(),
                       testcase_.problem())
