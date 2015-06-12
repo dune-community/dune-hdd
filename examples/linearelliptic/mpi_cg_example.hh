57bc26f629a4184d88f430515e9421089d8e216d
Rene Milk <rene.milk@uni-muenster.de>
2015-06-12 17:18:26 +0200

    [mpi ex.] simpler example switching

--- examples/linearelliptic/mpi_cg_example.hh
+++ examples/linearelliptic/mpi_cg_example.hh
@@ -19,7 +19,7 @@
 #include <dune/hdd/linearelliptic/problems/random_block_problem.hh>
 #include <dune/hdd/linearelliptic/testcases/random_block_testcase.hh>
 
-class MpiCGExample
+class MpiCGExampleThermal
 {
 public:
   static constexpr int dimDomain = 2;
@@ -31,24 +31,13 @@ public:
   static constexpr unsigned int dimRange = 1;
   typedef typename DSG::Entity<typename GridType::LeafGridView>::Type EntityType;
 
-//  typedef Dune::HDD::LinearElliptic::Problems::ESV2007
-//      < EntityType, RangeFieldType, dimRange, RangeFieldType, dimRange> ProblemType;
-//  typedef Dune::HDD::LinearElliptic::TestCases::ESV2007
-//      < GridType > TestcaseType;
-//  typedef Dune::HDD::LinearElliptic::TestCases::Thermalblock
-//      < GridType > TestcaseType;
-  typedef Dune::HDD::LinearElliptic::TestCases::RandomBlockTestcase
+  typedef Dune::HDD::LinearElliptic::TestCases::Thermalblock
       < GridType > TestcaseType;
-
-//  typedef Dune::HDD::LinearElliptic::Problems::Spe10::Model1
-//      < EntityType, RangeFieldType, dimRange, RangeFieldType, dimRange> ProblemType;
-//  typedef Dune::HDD::LinearElliptic::TestCases::Spe10::ParametricModel1
-//      < GridType > TestcaseType;
-    typedef Dune::HDD::LinearElliptic::Problems::RandomBlockProblem
-        < EntityType, RangeFieldType, dimRange, RangeFieldType, dimRange> ProblemType;
+  typedef Dune::HDD::LinearElliptic::TestCases::Spe10::ParametricModel1
+      < GridType > ProblemType;
 
 public:
-  MpiCGExample(const std::size_t num_refinements,
+  MpiCGExampleThermal(const std::size_t num_refinements,
                const unsigned int overlap_size,
                DSC::FieldVector< size_t, dimDomain > num_blocks
                = DSC::ValueInitFieldVector< size_t, dimDomain, 2u >())
@@ -80,5 +69,57 @@ private:
   DiscretizationType discretization_;
 }; // class LinearellipticExampleCG
 
+class MpiCGExampleRandom
+{
+public:
+  static constexpr int dimDomain = 2;
+  typedef double RangeFieldType;
+  typedef Dune::HDD::LinearElliptic::Discretizations::MpiCG<dimDomain>
+       DiscretizationType;
+  typedef Dune::SPGrid<RangeFieldType, dimDomain> GridType;
+  typedef Dune::Stuff::Grid::Providers::Cube< GridType > GridProviderType;
+  static constexpr unsigned int dimRange = 1;
+  typedef typename DSG::Entity<typename GridType::LeafGridView>::Type EntityType;
+
+  typedef Dune::HDD::LinearElliptic::TestCases::RandomBlockTestcase
+      < GridType > TestcaseType;
+  typedef Dune::HDD::LinearElliptic::Problems::RandomBlockProblem
+      < EntityType, RangeFieldType, dimRange, RangeFieldType, dimRange> ProblemType;
+
+public:
+  MpiCGExampleRandom(const std::size_t num_refinements,
+               const unsigned int overlap_size,
+               DSC::FieldVector< size_t, dimDomain > num_blocks
+               = DSC::ValueInitFieldVector< size_t, dimDomain, 2u >())
+    : testcase_( num_refinements, num_blocks, overlap_size)
+    , discretization_(testcase_,
+                      testcase_.boundary_info(),
+                      testcase_.problem())
+  {
+    discretization_.init(DSC_LOG_DEBUG_0);
+  } // MpiCGExample(...)
+
+  static std::string static_id()
+  {
+    return "mpi_cg";
+  }
+
+  const DiscretizationType& discretization() const
+  {
+    return discretization_;
+  }
+
+  DiscretizationType* discretization_and_return_ptr() const
+  {
+    return new DiscretizationType(discretization_);
+  }
+
+private:
+  TestcaseType testcase_;
+  DiscretizationType discretization_;
+}; // class LinearellipticExampleCG
+
+//typedef MpiCGExampleRandom MpiCGExample;
+typedef MpiCGExampleThermal MpiCGExample;
 
 #endif // CG_MPI_HH
