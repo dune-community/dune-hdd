c31968bcf05aa2ee9a2d0a618ebfeb687d451de7
Felix Schindler <felix.schindler@wwu.de>
2015-11-11 19:42:52 +0100

    [examples.linearparabolic.thermalblock] 2d

--- examples/linearparabolic/thermalblock_main.py
+++ examples/linearparabolic/thermalblock_main.py
@@ -14,20 +14,19 @@ from thermalblock_prepare import prepare
 
 
 config = {'dune_num_blocks': '[2 2 1]',
-          'dune_num_grid_elements': '[512 512 512]',
+          'dune_num_grid_elements': '[32 32 32]',
           'dune_products': ['l2', 'h1', 'elliptic'],
           'dune_log_info_level': 99,
           'dune_log_debug_level': -1,
           'dune_log_enable_warnings': True,
-          'dune_example': 'CG_Thermalblock_1dYaspGrid_fem_eigen',
+          'dune_example': 'CG_Thermalblock_2dYaspGrid_fem_eigen',
           'parameter_range': (0.1, 10),
-          'initial_values_expr': 'exp(-((x[0]-0.5)*(x[0]-0.5))/(2*0.1*0.1))',
+          'initial_values_expr': 'exp(-((x[0]-0.5)*(x[0]-0.5)*(x[1]-0.5)*(x[1]-0.5))/(2*0.01*0.01))',
           'end_time': 0.075,
           'num_timesteps': 250}
 
 pymor.core.logger.MAX_HIERACHY_LEVEL = 2
 getLogger('pymor.algorithms').setLevel('INFO')
-getLogger('pymor.la.genericsolvers.lgmres').setLevel('WARN')
 getLogger('pymor.WrappedDiscretization').setLevel('WARN')
 getLogger('dune.pymor.discretizations').setLevel('WARN')
 
@@ -38,8 +37,6 @@ if __name__ == '__main__':
     print('')
 
     discretization = detailed_data['discretization']
-    u = discretization.solve([1, 1])
+    u = discretization.solve([1, 0.1, 0.1, 1])
     discretization.visualize(u, filename='temperature__diffusion_1')
-    u = discretization.solve([0.1, 10])
-    discretization.visualize(u, filename='temperature__diffusion_01_10')
 
