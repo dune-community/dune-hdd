5ac43a8414d76609402005acacf81da2f6b14924
Felix Schindler <felix.schindler@wwu.de>
2015-12-09 19:19:24 +0100

    [examples...battery] udate

--- examples/linearparabolic/battery.py
+++ examples/linearparabolic/battery.py
@@ -45,7 +45,7 @@ class InstationaryDuneVisualizer(object):
                                 False) # do not add dirichlet shift
 
 
-logger.info('initializing DUNE module:')
+logger.info('initializing DUNE module ...')
 from generic import dune_module, examples, wrapper
 Example = examples[3]['aluconformgrid']['fem']['istl']
 
@@ -56,14 +56,19 @@ logger_cfg.set('info_color', 'blue', True)
 # multibat-pymor/problem-data/46x20x20_h4e-6m
 battery_geometry = {'lower_left':   '[0      0     0]',
                     'upper_right':  '[0.0184 0.008 0.008]',
-                    'num_elements': '[46     20    20]'}
+                    'num_elements': '[46     20    20]',
+                    'filename': 'geometry__46x20x20_h4e-6m'}
+# multibat-pymor/problem-data/ellisoid_5_5_16.8
+# battery_geometry = {'lower_left':   '[0      0     0]',
+#                     'upper_right':  '[0.0246 0.006 0.006]',
+#                     'num_elements': '[246    60    60]',
+#                     'filename': 'geometry__ellisoid_5_5_16.8'}
 
 grid_cfg = Example.grid_options('stuff.grid.provider.cube')
 grid_cfg.set('lower_left', battery_geometry['lower_left'], True)
 grid_cfg.set('upper_right', battery_geometry['upper_right'], True)
 grid_cfg.set('num_elements', battery_geometry['num_elements'], True)
 
-# boundary_cfg = Example.boundary_options('stuff.grid.boundaryinfo.alldirichlet')
 boundary_cfg = dune_module.Dune.Stuff.Common.Configuration()
 boundary_cfg.set('type', 'stuff.grid.boundaryinfo.normalbased')
 boundary_cfg.set('default', 'neumann')
@@ -72,6 +77,10 @@ boundary_cfg.set('dirichlet.1', '[1 0 0]')
 
 dirichlet_value = '298'
 problem_cfg = Example.problem_options('hdd.linearelliptic.problem.battery')
+problem_cfg.set('diffusion_factor.lower_left', battery_geometry['lower_left'], True)
+problem_cfg.set('diffusion_factor.upper_right', battery_geometry['upper_right'], True)
+problem_cfg.set('diffusion_factor.num_elements', battery_geometry['num_elements'], True)
+problem_cfg.set('diffusion_factor.filename', battery_geometry['filename'], True)
 problem_cfg.set('dirichlet.expression', dirichlet_value, True)
 problem_cfg.set('neumann.expression', '0', True)
 problem_cfg.set('force.value', '1000', True)
@@ -79,62 +88,65 @@ problem_cfg.set('force.value', '1000', True)
 # all but the 'type' will be discarded, so no point in setting more details here
 solver_options = Example.solver_options('bicgstab.amg.ilu0')
 
-example = Example(logger_cfg, grid_cfg, boundary_cfg, problem_cfg)
-stationary_discretization = wrapper[example.discretization()]
-stationary_discretization.unlock()
-stationary_discretization.solver_options = solver_options
-stationary_discretization.lock()
+end_time = 0.01
+nt = 100
 
-logger.info('  discretization has {} DoFs'.format(stationary_discretization.solution_space.dim))
-logger.info('  parameter type is {}'.format(stationary_discretization.parameter_type))
+example = Example(logger_cfg, grid_cfg, boundary_cfg, problem_cfg)
+disc = wrapper[example.discretization()]
+logger.info('  discretization has {} DoFs'.format(disc.solution_space.dim))
+logger.info('  parameter type is {}'.format(disc.parameter_type))
 logger.info('visualizing grid and data functions ...')
 example.visualize(problem_cfg.get_str('type'))
 
 logger.info('projecting initial values ...')
-dirichlet_shift = stationary_discretization.vector_operators['dirichlet'].as_vector()
+dirichlet_shift = disc.vector_operators['dirichlet'].as_vector()
 initial_values = make_listvectorarray(wrapper[example.project(dirichlet_value)])
 initial_values -= dirichlet_shift
 
-end_time = 0.01
-nt = 10
-discretization = InstationaryDiscretization(T=end_time,
-                                            initial_data=initial_values,
-                                            operator=stationary_discretization.operator,
-                                            rhs=stationary_discretization.rhs,
-                                            mass=stationary_discretization.products['l2_0'],
-                                            time_stepper=ImplicitEulerTimeStepper(nt, invert_options=solver_options.get_str('type')),
-                                            products=stationary_discretization.products,
-                                            operators=stationary_discretization.operators,
-                                            functionals=stationary_discretization.functionals,
-                                            vector_operators=stationary_discretization.vector_operators,
-                                            visualizer=InstationaryDuneVisualizer(stationary_discretization,
-                                                                                  problem_cfg.get_str('type') + '.solution'),
-                                            parameter_space=CubicParameterSpace(stationary_discretization.parameter_type,
-                                                                                (0.1,),
-                                                                                (10,)),
-                                            cache_region='disk')
+disc = InstationaryDiscretization(T=end_time,
+                                  initial_data=initial_values,
+                                  operator=disc.operator,
+                                  rhs=disc.rhs,
+                                  mass=disc.products['l2_0'],
+                                  time_stepper=ImplicitEulerTimeStepper(nt, invert_options=solver_options.get_str('type')),
+                                  products=disc.products,
+                                  operators=disc.operators,
+                                  functionals=disc.functionals,
+                                  vector_operators=disc.vector_operators,
+                                  visualizer=InstationaryDuneVisualizer(disc,
+                                                                        problem_cfg.get_str('type') + '.solution'),
+                                  parameter_space=CubicParameterSpace(disc.parameter_type, 0.1, 10),
+                                  cache_region='disk')
 
-mu = {'ELECTROLYTE': 0.6}
+mu = 0.6
+U = disc.solve(mu)
+# U += dirichlet_shift
+# logger.info('visualizing trajectory ...')
+# disc.visualize(U)
+
 num_training_samples = 1
-max_rb_size = 1
-greedy_data = greedy(discretization,
-                     reduce_generic_rb,
-                     # discretization.parameter_space.sample_uniformly(num_training_samples),
+max_rb_size = 100
+
+def norm(U):
+    return np.max(disc.h1_0_norm(U))
+
+def reductor(discretization, RB, vector_product=None, disable_caching=True, extends=None):
+    rd, rc, reduction_data = reduce_generic_rb(discretization, RB, vector_product, disable_caching,
+            extends)
+    return rd.with_(time_stepper=ImplicitEulerTimeStepper(nt)), rc, reduction_data
+
+greedy_data = greedy(disc,
+                     reductor,
+                     # disc.parameter_space.sample_uniformly(num_training_samples),
                      (mu,),
                      use_estimator=False,
-                     error_norm=lambda U: np.max(discretization.h1_0_norm(U)),
-                     extension_algorithm=partial(pod_basis_extension, count=1, product=discretization.products['h1_0']),
+                     error_norm=norm,
+                     extension_algorithm=partial(pod_basis_extension, count=1, product=disc.products['h1_0']),
                      max_extensions=max_rb_size)
-
 rd, rc = greedy_data['reduced_discretization'], greedy_data['reconstructor']
-rd = rd.with_(time_stepper=ImplicitEulerTimeStepper(nt))
-URB = rd.solve(mu)
-U = discretization.solve(mu)
-print('error: {}'.format(np.max(discretization.h1_0_norm(U - rc.reconstruct(URB)))))
 
-U += dirichlet_shift
-logger.info('visualizing trajectory ...')
-discretization.visualize(U)
+URB = rd.solve(mu)
+print('error: {}'.format(norm(U - rc.reconstruct(URB))))
 
 logger.info(' ')
 
