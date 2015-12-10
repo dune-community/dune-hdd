00fba32dda103d63873571556378e90d136426cb
Felix Schindler <felix.schindler@wwu.de>
2015-12-10 18:34:55 +0100

    [examples...generic] minor update

--- examples/linearelliptic/generic.hh
+++ examples/linearelliptic/generic.hh
@@ -156,7 +156,7 @@ public:
       DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Given expression must not be empty!");
     auto logger = DSC::TimedLogger().get("example.linearelliptic.generic.project");
     logger.info() << "projecting '" << expression << "'... " << std::flush;
-    auto discrete_function = GDT::make_discrete_function< VectorType >(discretization_->ansatz_space());
+    auto discrete_function = GDT::make_discrete_function< VectorType >(*discretization_->ansatz_space());
     GDT::Operators::apply_projection(Stuff::Functions::Expression< E, D, d, R, r >("x", expression), discrete_function);
     logger.info() << "done" << std::endl;
     return discrete_function.vector();
