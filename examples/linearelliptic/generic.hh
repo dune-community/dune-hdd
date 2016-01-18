97c8fa35a4ad4c9b919c5c7c2f11011310385a94
Felix Schindler <felix.schindler@wwu.de>
2016-01-18 11:19:21 +0100

    [examples...generic] fix space usage

--- examples/linearelliptic/generic.hh
+++ examples/linearelliptic/generic.hh
@@ -133,7 +133,7 @@ public:
                                                              -1);
 //                                                             /*only_these_products=*/std::vector<std::string>({"l2", "h1", "elliptic"}));
     discretization_->init();
-    logger.info() << "done (has " << discretization_->ansatz_space()->mapper().size() << " DoFs)" << std::endl;
+    logger.info() << "done (has " << discretization_->ansatz_space().mapper().size() << " DoFs)" << std::endl;
   } // GenericLinearellipticExample(...)
 
   DiscretizationType& discretization()
@@ -156,7 +156,7 @@ public:
       DUNE_THROW(Stuff::Exceptions::wrong_input_given, "Given expression must not be empty!");
     auto logger = DSC::TimedLogger().get("example.linearelliptic.generic.project");
     logger.info() << "projecting '" << expression << "'... " << std::flush;
-    auto discrete_function = GDT::make_discrete_function< VectorType >(*discretization_->ansatz_space());
+    auto discrete_function = GDT::make_discrete_function< VectorType >(discretization_->ansatz_space());
     GDT::Operators::apply_projection(Stuff::Functions::Expression< E, D, d, R, r >("x", expression), discrete_function);
     logger.info() << "done" << std::endl;
     return discrete_function.vector();
