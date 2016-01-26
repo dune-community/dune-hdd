616a1cbd120a16f7c5cd8cfe0cbe0fe90b4e8311
Felix Schindler <felix.schindler@wwu.de>
2016-01-26 13:34:56 +0100

    [examples...generic] update bindings (disable pdelab due to SWIPDG)

--- examples/linearelliptic/generic_bindings_generator.py
+++ examples/linearelliptic/generic_bindings_generator.py
@@ -14,7 +14,7 @@ from dune.pymor.discretizations import inject_StationaryDiscretizationImplementa
 def inject_Example(module, exceptions, interfaces, CONFIG_H):
     '''injects the user code into the module'''
     ssize_t = CONFIG_H['DUNE_STUFF_SSIZE_T']
-    HAVE_DUNE_PDELAB = CONFIG_H['HAVE_DUNE_PDELAB']
+    HAVE_DUNE_PDELAB = False #CONFIG_H['HAVE_DUNE_PDELAB']
     HAVE_DUNE_FEM    = CONFIG_H['HAVE_DUNE_FEM']
     HAVE_DUNE_ISTL   = CONFIG_H['HAVE_DUNE_ISTL']
     HAVE_EIGEN       = CONFIG_H['HAVE_EIGEN']
@@ -133,15 +133,15 @@ def inject_Example(module, exceptions, interfaces, CONFIG_H):
     la_backend_istl  = 'Dune::Stuff::LA::ChooseBackend::istl_sparse'
     space_backend_pdelab = 'Dune::GDT::ChooseSpaceBackend::pdelab'
     space_backend_fem    = 'Dune::GDT::ChooseSpaceBackend::fem'
-    # if HAVE_DUNE_PDELAB and HAVE_DUNE_ISTL:
+    if HAVE_DUNE_PDELAB and HAVE_DUNE_ISTL:
         # add_example(YaspGrid1d, space_backend_pdelab, la_backend_istl, 'GenericLinearellipticExample_1dYaspGrid_pdelab_istl')
         # add_example(YaspGrid2d, space_backend_pdelab, la_backend_istl, 'GenericLinearellipticExample_2dYaspGrid_pdelab_istl')
-        # add_example(YaspGrid3d, space_backend_pdelab, la_backend_istl, 'GenericLinearellipticExample_3dYaspGrid_pdelab_istl')
-        # if HAVE_ALUGRID:
+        add_example(YaspGrid3d, space_backend_pdelab, la_backend_istl, 'GenericLinearellipticExample_3dYaspGrid_pdelab_istl')
+        if HAVE_ALUGRID:
             # add_example(AluGridConform2d, space_backend_pdelab, la_backend_istl,
             #             'GenericLinearellipticExample_2dAluConformGrid_pdelab_istl')
-            # add_example(AluGridConform3d, space_backend_pdelab, la_backend_istl,
-                        # 'GenericLinearellipticExample_3dAluConformGrid_pdelab_istl')
+            add_example(AluGridConform3d, space_backend_pdelab, la_backend_istl,
+                        'GenericLinearellipticExample_3dAluConformGrid_pdelab_istl')
         # if HAVE_DUNE_SPGRID:
         #     add_example(SPGrid1d, space_backend_pdelab, la_backend_istl,
         #                 'GenericLinearellipticExample_1dSpGrid_pdelab_istl')
@@ -149,15 +149,15 @@ def inject_Example(module, exceptions, interfaces, CONFIG_H):
         #                 'GenericLinearellipticExample_2dSpGrid_pdelab_istl')
         #     add_example(SPGrid3d, space_backend_pdelab, la_backend_istl,
         #                 'GenericLinearellipticExample_3dSpGrid_pdelab_istl')
-    # if HAVE_DUNE_PDELAB and HAVE_EIGEN:
+    if HAVE_DUNE_PDELAB and HAVE_EIGEN:
     #     add_example(YaspGrid1d, space_backend_pdelab, la_backend_eigen, 'GenericLinearellipticExample_1dYaspGrid_pdelab_eigen')
     #     add_example(YaspGrid2d, space_backend_pdelab, la_backend_eigen, 'GenericLinearellipticExample_2dYaspGrid_pdelab_eigen')
-    #     add_example(YaspGrid3d, space_backend_pdelab, la_backend_eigen, 'GenericLinearellipticExample_3dYaspGrid_pdelab_eigen')
-    #     if HAVE_ALUGRID:
+        add_example(YaspGrid3d, space_backend_pdelab, la_backend_eigen, 'GenericLinearellipticExample_3dYaspGrid_pdelab_eigen')
+        if HAVE_ALUGRID:
     #         add_example(AluGridConform2d, space_backend_pdelab, la_backend_eigen,
     #                     'GenericLinearellipticExample_2dAluConformGrid_pdelab_eigen')
-    #         add_example(AluGridConform3d, space_backend_pdelab, la_backend_eigen,
-    #                     'GenericLinearellipticExample_3dAluConformGrid_pdelab_eigen')
+            add_example(AluGridConform3d, space_backend_pdelab, la_backend_eigen,
+                        'GenericLinearellipticExample_3dAluConformGrid_pdelab_eigen')
     #     if HAVE_DUNE_SPGRID:
     #         add_example(SPGrid1d, space_backend_pdelab, la_backend_eigen,
     #                     'GenericLinearellipticExample_1dSpGrid_pdelab_eigen')
@@ -181,15 +181,15 @@ def inject_Example(module, exceptions, interfaces, CONFIG_H):
     #                     'GenericLinearellipticExample_2dSpGrid_fem_istl')
     #         add_example(SPGrid3d, space_backend_fem, la_backend_istl,
     #                     'GenericLinearellipticExample_3dSpGrid_fem_istl')
-    # if HAVE_DUNE_FEM and HAVE_EIGEN:
+    if HAVE_DUNE_FEM and HAVE_EIGEN:
     #     add_example(YaspGrid1d, space_backend_fem, la_backend_eigen, 'GenericLinearellipticExample_1dYaspGrid_fem_eigen')
     #     add_example(YaspGrid2d, space_backend_fem, la_backend_eigen, 'GenericLinearellipticExample_2dYaspGrid_fem_eigen')
-    #     add_example(YaspGrid3d, space_backend_fem, la_backend_eigen, 'GenericLinearellipticExample_3dYaspGrid_fem_eigen')
-    #     if HAVE_ALUGRID:
+        add_example(YaspGrid3d, space_backend_fem, la_backend_eigen, 'GenericLinearellipticExample_3dYaspGrid_fem_eigen')
+        if HAVE_ALUGRID:
     #         add_example(AluGridConform2d, space_backend_fem, la_backend_eigen,
     #                     'GenericLinearellipticExample_2dAluConformGrid_fem_eigen')
-    #         add_example(AluGridConform3d, space_backend_fem, la_backend_eigen,
-    #                     'GenericLinearellipticExample_3dAluConformGrid_fem_eigen')
+            add_example(AluGridConform3d, space_backend_fem, la_backend_eigen,
+                        'GenericLinearellipticExample_3dAluConformGrid_fem_eigen')
     #     if HAVE_DUNE_SPGRID:
     #         add_example(SPGrid1d, space_backend_fem, la_backend_eigen,
     #                     'GenericLinearellipticExample_1dSpGrid_fem_eigen')
